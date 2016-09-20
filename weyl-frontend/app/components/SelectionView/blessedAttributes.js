35ab6ee1e98e39e4143709d4f17bde2013c594eb
Oliver Charlesworth <oliver@quartic.io>
2016-09-20 14:24:42 +0100

    Add janky spec for blessed vs unblessed

---
+++ weyl-frontend/app/components/SelectionView/blessedAttributes.js
@@ -0,0 +1,17 @@
+// TODO: remove this jank
+const blessedAttributes = {
+  "London Boroughs": [
+    "hectares",
+    "political control in council",
+    "average age 2015",
+    "crime rates per thousand population 2014/15"
+
+  ],
+  "Postcode Districts": [
+    "gid",
+  ],
+};
+
+export {
+  blessedAttributes,
+};
