ac668f957a5fcccb1ddd5684aa3a282363bac306
Alex Sparrow <alex.sparrow@cern.ch>
2016-09-26 09:10:23 +0100

    Recording this stuff in case we want to return to it

--- weyl-frontend/app/components/GeofenceSettings/index.js
+++ weyl-frontend/app/components/GeofenceSettings/index.js
@@ -10,17 +10,38 @@ import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import styles from './styles.css';
 
+const GeofenceCreate = ({ visible }) => {
+    if (visible) {
+      return (
+        <div className={styles.geofenceCreate}>
+          <div className="ui form">
+            <div className="ui field">
+              <div className="ui fluid action input">
+                <input placeholder="Geofence Name..." type="text"/>
+                <button className="ui button mini icon"><i className="pencil icon"></i></button>
+                <button className="ui mini icon button"><i className="save icon"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    else {
+      return null;
+    }
+}
+
 const GeofenceList = ({ items }) =>
   (<div className="ui relaxed divided list">
     <div className="item">
-      <i className="large github middle aligned icon"></i>
+      <i className="large right floated middle aligned icon close"></i>
       <div className="content">
         <a className="header">Geofence 1</a>
         <div className="description">Updated 10 mins ago</div>
       </div>
     </div>
     <div className="item">
-      <i className="large github middle aligned icon"></i>
+      <i className="large right floated middle aligned icon close"></i>
       <div className="content">
         <a className="header">Geofence 2</a>
         <div className="description">Updated 22 mins ago</div>
@@ -30,6 +51,17 @@ const GeofenceList = ({ items }) =>
 );
 
 class GeofenceSettings extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+    this.state = {
+      creating: false,
+    };
+  }
+
+  onNewClick() {
+    this.setState({ creating: ! this.state.creating });
+  }
+
   render() {
     return (
       <div className={styles.geofenceSettings}>
@@ -37,8 +69,9 @@ class GeofenceSettings extends React.Component { // eslint-disable-line react/pr
           <i className="icon close"></i>
         </a><b>Geofence</b>
         <div className="right floated">
-          <button className="ui mini button primary">New</button>
+          <button className="ui mini button primary" onClick={() => this.onNewClick()}>New</button>
         </div>
+        <GeofenceCreate visible={this.state.creating}/>
         <div className="ui secondary segment">
           <GeofenceList items={[]}/>
         </div>
