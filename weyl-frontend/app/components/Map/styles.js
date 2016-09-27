389abb1b663458aa3a3e5ca2b238f549cf339952
Oliver Charlesworth <oliver@quartic.io>
2016-09-27 19:16:22 +0100

    WIP

--- weyl-frontend/app/components/Map/styles.js
+++ weyl-frontend/app/components/Map/styles.js
@@ -7,7 +7,7 @@ export function computeColorScaleFromBase(baseColor, step, n) {
   let color = baseColor;
   const result = [];
 
-  for (var i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     result.push(color);
     color = d3.hsl(color).darker(step);
     result.push(color.toString());
@@ -18,11 +18,10 @@ export function computeColorScaleFromBase(baseColor, step, n) {
 
 function computeStops(colorScale, nStops, minValue, maxValue) {
   const result = [];
-
   const colors = chroma.scale(colorScale).colors(nStops);
 
-  for (var i = 0; i < nStops; i++) {
-    result.push([minValue + (i * (maxValue - minValue) / nStops), colors[i].toString(0)]);
+  for (let i = 0; i < nStops; i++) {
+    result.push([minValue + ((i * (maxValue - minValue)) / nStops), colors[i].toString(0)]);
   }
   return result;
 }
@@ -43,57 +42,52 @@ function filter(property, geomType) {
     const propFilter = ["has", property];
     return ["all", geomFilter, propFilter];
   }
-  else {
-    return geomFilter;
-  }
+  return geomFilter;
 }
 
 export function buildStyleLayers(layer) {
-  let [layerName, style, attributeStats] = [layer.name, layer.style, layer.stats.attributeStats];
-  if (customStyles.hasOwnProperty(layerName)) {
+  const [layerName, style, attributeStats] = [layer.name, layer.style, layer.stats.attributeStats];
+  if (layerName in customStyles) {
     return customStyles[layerName];
-  }
-  else if (layer.live) {
+  } else if (layer.live) {
     return liveLayerStyle;
   }
-  else {
-    return {
-      point: {
-        type: "circle",
-        paint: {
-          "circle-radius": style.point["circle-radius"] - 2,
-          "circle-color": colorStyle(style.property, style.point, attributeStats),
-          "circle-opacity": style.opacity,
-        },
-        filter: filter(style.property, "Point"),
+  return {
+    point: {
+      type: "circle",
+      paint: {
+        "circle-radius": style.point["circle-radius"] - 2,
+        "circle-color": colorStyle(style.property, style.point, attributeStats),
+        "circle-opacity": style.opacity,
       },
-      point2: {
-        type: "circle",
-        paint: {
-          "circle-radius": style.point["circle-radius"],
-          "circle-color": "#FFFFFF",
-          "circle-opacity": style.opacity,
-        },
-        filter: filter(style.property, "Point"),
-        _zorder: 1,
+      filter: filter(style.property, "Point"),
+    },
+    point2: {
+      type: "circle",
+      paint: {
+        "circle-radius": style.point["circle-radius"],
+        "circle-color": "#FFFFFF",
+        "circle-opacity": style.opacity,
       },
-      polygon: {
-        type: "fill",
-        paint: {
-          "fill-color": colorStyle(style.property, style.polygon, attributeStats),
-          "fill-outline-color": style.property == null ? style.polygon["fill-outline-color"] : style.polygon.color,
-          "fill-opacity": style.opacity,
-        },
-        filter: filter(style.property, "Polygon"),
+      filter: filter(style.property, "Point"),
+      _zorder: 1,
+    },
+    polygon: {
+      type: "fill",
+      paint: {
+        "fill-color": colorStyle(style.property, style.polygon, attributeStats),
+        "fill-outline-color": style.property == null ? style.polygon["fill-outline-color"] : style.polygon.color,
+        "fill-opacity": style.opacity,
       },
-      line: {
-        type: "line",
-        paint: {
-          "line-color": colorStyle(style.property, style.line, attributeStats),
-          "line-width": 5,
-        },
-        filter: ["==", "$type", "LineString"],
+      filter: filter(style.property, "Polygon"),
+    },
+    line: {
+      type: "line",
+      paint: {
+        "line-color": colorStyle(style.property, style.line, attributeStats),
+        "line-width": 5,
       },
-    };
-  }
+      filter: ["==", "$type", "LineString"],
+    },
+  };
 }
