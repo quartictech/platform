c82e56de7056960cea516b9511717c32101b3108
Alex Sparrow <alex@quartic.io>
2016-09-27 14:08:38 +0000

    Fix indent

--- import.sh
+++ import.sh
@@ -17,19 +17,19 @@ curl -XPUT -H Content-Type:application/json $API_ROOT/layer/import -d '{
 	"name": "Companies",
  	"description": "All LTD companies in the UK",
  	"query": "SELECT companyname,geom from companies_geocoded limit 1000000"
- }'
+}'
 
 curl -XPUT -H Content-Type:application/json $API_ROOT/layer/import -d '{
  	"name": "Postcode Districts",
  	"description": "Postcode districts in the UK",
  	"query": "SELECT * from postcode_districts"
- }'
+}'
 
 curl -XPUT -H Content-Type:application/json $API_ROOT/layer/import -d '{
  	"name": "London Boroughs",
  	"description": "London Borough Boundaries",
  	"query": "SELECT lb.name, lb.geom, lbp.* from london_borough_excluding_mhw lb left join london_borough_profiles lbp on lb.name = lbp.AreaName"
- }'
+}'
 
 curl -XPUT -H Content-Type:application/json $API_ROOT/layer/import -d '{
 	"name": "London House Sales",
