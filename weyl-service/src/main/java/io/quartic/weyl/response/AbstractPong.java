e5a37efebf070fbe648c167f7c82633795a9133a
Oliver Charlesworth <oliver@quartic.io>
2016-10-06 17:17:13 +0100

    Add version to /ping response

---
+++ weyl-service/src/main/java/io/quartic/weyl/response/AbstractPong.java
@@ -0,0 +1,10 @@
+package io.quartic.weyl.response;
+
+import io.quartic.weyl.core.geojson.SweetStyle;
+import org.immutables.value.Value;
+
+@SweetStyle
+@Value.Immutable
+public interface AbstractPong {
+    String version();
+}
