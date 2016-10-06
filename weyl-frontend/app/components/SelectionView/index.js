c8d3fc0cf78d0cf018423dcec604936dc1edf99a
Oliver Charlesworth <oliver@quartic.io>
2016-10-06 14:05:58 +0100

    WIP on side-by-side

--- weyl-frontend/app/components/SelectionView/index.js
+++ weyl-frontend/app/components/SelectionView/index.js
@@ -8,30 +8,20 @@ import { defaultBehavior, curatedBehaviors } from "./behaviors";
 
 class SelectionView extends React.Component { // eslint-disable-line react/prefer-stateless-function
   renderInner() {
-    if ((this.props.selection.length === 0)
-      || !this.props.selection[0].layer.visible) {
+    const filteredSelection = this.props.selection
+      .filter(s => s.layer.visible);  // TODO: take account of actual filtering
+
+    if (filteredSelection.length === 0) {
       return null;
-    }
-
-    const numUniqueLayers = _.chain(this.props.selection)
-      .map(s => s.layer.id).uniq().size().value();
-    const numFeatures = this.props.selection.length;
-
-    if (numFeatures === 1) {
-      console.log("Single object");
-    } else if (numFeatures === 2 && numUniqueLayers === 1) {
-      console.log("Side-by-side comparison");
-    } else {
-      console.log("Aggregates");
     }
 
     return (
       <div className={styles.innerSelectionView}>
         <div className="ui raised fluid card">
           <div className="content">
-            <Header selection={this.props.selection} onClose={this.props.onClose} />
-            <Media selection={this.props.selection} />
-            <BlessedProperties selection={this.props.selection} />
+            <Header selection={filteredSelection} onClose={this.props.onClose} />
+            <Media selection={filteredSelection} />
+            <BlessedProperties selection={filteredSelection} />
           </div>
 
           <div className="extra content">
@@ -42,7 +32,7 @@ class SelectionView extends React.Component { // eslint-disable-line react/prefe
               </div>
 
               <div className="content">
-                <UnblessedProperties selection={this.props.selection} />
+                <UnblessedProperties selection={filteredSelection} />
               </div>
 
             </div>
@@ -94,14 +84,17 @@ const BlessedProperties = ({ selection }) => {
   const properties = selection[0].properties;
   const layerName = selection[0].layer.metadata.name;
 
+  const numUniqueLayers = _.chain(selection).map(s => s.layer.id).uniq().size().value();
+  const numFeatures = selection.length;
+
   return (
     <div className="ui segment">
       <PropertiesTable
-        properties={properties}
+        selection={selection}
         order={
           isAnythingBlessed(layerName)
-            ? getBlessedPropertyOrder(layerName, properties)
-            : getUnblessedPropertyOrder(layerName, properties)
+            ? getBlessedPropertyOrder(layerName, selection[0].properties) // TODO: union properties
+            : getUnblessedPropertyOrder(layerName, selection[0].properties) // TODO: union properties
         }
       />
     </div>
@@ -112,39 +105,56 @@ const UnblessedProperties = ({ selection }) => {
   const properties = selection[0].properties;
   const layerName = selection[0].layer.metadata.name;
 
-  return (
-    <div className="ui secondary segment">
-      <PropertiesTable
-        properties={properties}
-        order={
-          isAnythingBlessed(layerName)
-            ? getUnblessedPropertyOrder(layerName, properties)
-            : getBlessedPropertyOrder(layerName, properties)
-        }
-      />
-    </div>
-  );
+  // TODO
+  return <div></div>;
+
+  // return (
+  //   <div className="ui secondary segment">
+  //     <PropertiesTable
+  //       properties={properties}
+  //       order={
+  //         isAnythingBlessed(layerName)
+  //           ? getUnblessedPropertyOrder(layerName, properties)
+  //           : getBlessedPropertyOrder(layerName, properties)
+  //       }
+  //     />
+  //   </div>
+  // );
 };
 
-const PropertiesTable = ({ properties, order }) => (
-  <table className="ui very basic celled very compact fixed selectable table">
+const PropertiesTable = ({ selection, order }) => (
+  <table className="ui very basic celled very compact small fixed selectable table">
+    {
+      (selection.length > 1) &&
+        <thead>
+          <tr>
+            <th />
+            {selection.map(s =>
+              <th key={s.properties["_id"]}>{getTitle(s.layer.metadata.name, s.properties)}</th>  // eslint-disable-line dot-notation
+            )}
+          </tr>
+        </thead>
+    }
     <tbody>
       {order
-        .filter(key => key !== "_id")
-        .filter(key => key in properties)
-        .filter(key => String(properties[key]).trim() !== "")
+        .filter(key => _.some(selection, s => isPropertyDisplayable(key, s.properties)))
         .map(key =>
           <tr key={key}>
             <td className="right aligned">
               <div className="ui sub header">{key}</div>
             </td>
-            <td>{properties[key]}</td>
+            {selection.map(s =>
+              <td key={s.properties["_id"]}>{s.properties[key]}</td>  // eslint-disable-line dot-notation
+            )}
           </tr>
         )
       }
     </tbody>
   </table>
 );
+
+const isPropertyDisplayable = (key, properties) =>
+  (key !== "_id") && (key in properties) && (String(properties[key]).trim() !== "");
 
 const getTitle = (layerName, properties) =>
   getBehavior(layerName).title(properties);
