fd900c8036a11f4654e2ce825febf5a6fcb544e9
Oliver Charlesworth <oliver@quartic.io>
2016-10-11 23:27:20 +0100

    Add icons to static layers

--- weyl-service/src/main/java/io/quartic/weyl/request/PostgisImportRequest.java
+++ weyl-service/src/main/java/io/quartic/weyl/request/PostgisImportRequest.java
@@ -1,14 +1,16 @@
 package io.quartic.weyl.request;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.quartic.weyl.core.model.LayerMetadata;
 import org.immutables.value.Value;
 
 @Value.Immutable
 @JsonSerialize(as=ImmutablePostgisImportRequest.class)
 @JsonDeserialize(as=ImmutablePostgisImportRequest.class)
 public interface PostgisImportRequest {
-    String name();
-    String description();
+    @JsonUnwrapped
+    LayerMetadata metadata();
     String query();
 }
