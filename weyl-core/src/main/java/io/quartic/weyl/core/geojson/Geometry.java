6343489aa57c12488cee067a83171cc38a6dccfc
Oliver Charlesworth <oliver@quartic.io>
2016-10-11 18:23:35 +0100

    Add MultiPolygon and 'handling' for CRS

--- weyl-core/src/main/java/io/quartic/weyl/core/geojson/Geometry.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/geojson/Geometry.java
@@ -3,12 +3,19 @@ package io.quartic.weyl.core.geojson;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.immutables.value.Value;
+
+import java.util.Map;
+import java.util.Optional;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property="type")
 @JsonSubTypes({
         @Type(value = Point.class, name = "Point"),
         @Type(value = LineString.class, name = "LineString"),
-        @Type(value = Polygon.class, name = "Polygon")
+        @Type(value = Polygon.class, name = "Polygon"),
+        @Type(value = MultiPolygon.class, name = "MultiPolygon")
 })
 public interface Geometry {
+    @Value.Parameter(false)
+    Optional<Map<String, Object>> crs();
 }
--- weyl-core/src/main/java/io/quartic/weyl/core/geojson/Utils.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/geojson/Utils.java
@@ -1,7 +1,5 @@
 package io.quartic.weyl.core.geojson;
-
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LinearRing;
@@ -12,16 +10,12 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static java.util.Arrays.asList;
-
 public final class Utils {
     private static final GeometryFactory factory = new GeometryFactory();
-
     private Utils() {}
-
     public static LineString lineStringFrom(Point... points) {
         return lineStringFrom(asList(points));
     }
-
     public static LineString lineStringFrom(Iterable<Point> points) {
         return LineString.of(
                 StreamSupport.stream(points.spliterator(), true)
@@ -29,7 +23,6 @@ public final class Utils {
                         .collect(Collectors.toList())
         );
     }
-
     public static com.vividsolutions.jts.geom.Geometry toJts(Geometry geometry) {
         // TODO: this is gross - can we use a visitor?
         if (geometry instanceof Point) {
@@ -42,17 +35,24 @@ public final class Utils {
         }
         if (geometry instanceof Polygon) {
             Polygon polygon = (Polygon) geometry;
-            LinearRing exterior  =factory.createLinearRing(listToCoords(polygon.coordinates().get(0)));
-            LinearRing[] holes =polygon.coordinates().stream().skip(1)
-                    .map(Utils::listToCoords)
-                    .map(factory::createLinearRing)
-                    .toArray(LinearRing[]::new);
-
-            return factory.createPolygon(exterior, holes);
+            return createPolygon(polygon.coordinates());
+        }
+        if (geometry instanceof MultiPolygon) {
+            MultiPolygon multiPolygon = (MultiPolygon) geometry;
+            com.vividsolutions.jts.geom.Polygon[] polygons = multiPolygon.coordinates().stream().map(Utils::createPolygon)
+                    .toArray(com.vividsolutions.jts.geom.Polygon[]::new);
+            return factory.createMultiPolygon(polygons);
         }
         throw new UnsupportedOperationException("Cannot convert from type " + geometry.getClass().getCanonicalName());
     }
-
+    private static com.vividsolutions.jts.geom.Polygon createPolygon(List<List<List<Double>>> coordinates) {
+        LinearRing exterior = factory.createLinearRing(listToCoords(coordinates.get(0)));
+        LinearRing[] holes = coordinates.stream().skip(1)
+                .map(Utils::listToCoords)
+                .map(factory::createLinearRing)
+                .toArray(LinearRing[]::new);
+        return factory.createPolygon(exterior, holes);
+    }
     public static Geometry fromJts(com.vividsolutions.jts.geom.Geometry geometry) {
         // TODO: this is gross - can we use a visitor?
         if (geometry instanceof com.vividsolutions.jts.geom.Point) {
@@ -65,24 +65,20 @@ public final class Utils {
         }
         throw new UnsupportedOperationException("Cannot convert from type " + geometry.getClass().getCanonicalName());
     }
-
     private static Coordinate[] listToCoords(List<List<Double>> list) {
         return list.stream()
                 .map(Utils::listToCoord)
                 .collect(Collectors.toList())
                 .toArray(new Coordinate[0]);
     }
-
     private static Coordinate listToCoord(List<Double> list) {
         return new Coordinate(list.get(0), list.get(1));
     }
-
     private static List<List<Double>> coordsToList(Coordinate[] coordinates) {
         return Arrays.stream(coordinates)
                 .map(Utils::coordToList)
                 .collect(Collectors.toList());
     }
-
     private static List<Double> coordToList(Coordinate coordinate) {
         return ImmutableList.of(coordinate.x, coordinate.y);
     }
