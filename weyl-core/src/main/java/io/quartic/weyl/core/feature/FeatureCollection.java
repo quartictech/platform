9818d4bfafc8bb936d57d182bc6da9d70207be54
Oliver Charlesworth <oliver@quartic.io>
2016-10-11 17:48:47 +0100

    Avoid cubic complexity

--- weyl-core/src/main/java/io/quartic/weyl/core/feature/FeatureCollection.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/feature/FeatureCollection.java
@@ -9,7 +9,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import static com.google.common.collect.ImmutableList.copyOf;
-import static com.google.common.collect.Iterables.concat;
 
 public class FeatureCollection extends AbstractCollection<Feature> {
     public interface Store {
@@ -17,27 +16,61 @@ public class FeatureCollection extends AbstractCollection<Feature> {
     }
 
     private final Store store;
+    private final FeatureCollection prev;
     private final Iterable<Feature> features;
     private final int size;
 
     FeatureCollection(Store store) {
-        this(store, ImmutableList.of(), 0);
+        this(store, null, ImmutableList.of(), 0);
     }
 
-    private FeatureCollection(Store store, Iterable<Feature> features, int size) {
+    private FeatureCollection(Store store, FeatureCollection prev, Iterable<Feature> features, int size) {
         this.store = store;
+        this.prev = prev;
         this.features = features;
         this.size = size;
     }
 
     public FeatureCollection append(List<Feature> features) {
         store.addAll(features);
-        return new FeatureCollection(store, concat(this, copyOf(features)), size + features.size());
+        return new FeatureCollection(store, this, copyOf(features), size + features.size());
     }
 
     @Override
     public Iterator<Feature> iterator() {
-        return features.iterator();
+        return new Iterator<Feature>() {
+            FeatureCollection current = FeatureCollection.this;
+            Iterator<Feature> currentIterator = current.features.iterator();
+
+            {
+                advance();
+            }
+
+            @Override
+            public boolean hasNext() {
+                return (current != null);
+            }
+
+            @Override
+            public Feature next() {
+                final Feature feature = currentIterator.next();
+                advance();
+                return feature;
+            }
+
+            private void advance() {
+                while (true) {
+                    if (currentIterator.hasNext()) {
+                        return;
+                    }
+                    current = current.prev;
+                    if (current == null) {
+                        return;
+                    }
+                    currentIterator = current.features.iterator();
+                }
+            }
+        };
     }
 
     @Override
