fb5103659c846894c9d9a7fb0f81f8ce70dfbe4d
Oliver Charlesworth <oliver@quartic.io>
2016-10-16 15:25:30 +0100

    Extract StatsCalcultor

--- weyl-core/src/main/java/io/quartic/weyl/core/LayerStore.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/LayerStore.java
@@ -20,6 +20,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static io.quartic.weyl.core.StatsCalculator.calculateStats;
+
 public class LayerStore {
     private static final Logger log = LoggerFactory.getLogger(LayerStore.class);
     private final Map<LayerId, IndexedLayer> indexedLayers = Maps.newConcurrentMap();
@@ -94,48 +96,6 @@ public class LayerStore {
                  .layerId(lidGenerator.get())
                  .layerStats(calculateStats(layer))
                  .build();
-    }
-
-    private static LayerStats calculateStats(AbstractLayer layer) {
-        Map<String, Double> maxNumeric = Maps.newConcurrentMap();
-        Map<String, Double> minNumeric = Maps.newConcurrentMap();
-
-        AttributeSchema attributeSchema = layer.schema();
-
-        layer.features().parallelStream()
-                .flatMap(feature -> feature.metadata().entrySet().stream())
-                .filter(entry -> attributeSchema.attributes().get(entry.getKey()).type()
-                        == AttributeType.NUMERIC)
-                .forEach(entry -> {
-                            Object value = entry.getValue();
-                            double doubleValue = Double.valueOf(value.toString());
-
-                            if (!maxNumeric.containsKey(entry.getKey())) {
-                                maxNumeric.put(entry.getKey(), doubleValue);
-                            } else if (doubleValue > maxNumeric.get(entry.getKey())) {
-                                maxNumeric.put(entry.getKey(), doubleValue);
-                            }
-                            if (!minNumeric.containsKey(entry.getKey())) {
-                                minNumeric.put(entry.getKey(), doubleValue);
-                            } else if (doubleValue < minNumeric.get(entry.getKey())) {
-                                minNumeric.put(entry.getKey(), doubleValue);
-                            }
-                        });
-
-        ImmutableLayerStats.Builder builder = ImmutableLayerStats.builder();
-        layer.schema().attributes()
-                .entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().type() == AttributeType.NUMERIC)
-                .forEach(entry -> builder.putAttributeStats(entry.getKey(),
-                        ImmutableAttributeStats.builder()
-                                .minimum(minNumeric.get(entry.getKey()))
-                                .maximum(maxNumeric.get(entry.getKey()))
-                        .build()));
-
-        builder.featureCount(layer.features().size());
-
-        return builder.build();
     }
 
     private static SpatialIndex spatialIndex(Collection<IndexedFeature> features) {
---
+++ weyl-core/src/main/java/io/quartic/weyl/core/StatsCalculator.java
@@ -0,0 +1,50 @@
+package io.quartic.weyl.core;
+
+import com.google.common.collect.Maps;
+import io.quartic.weyl.core.model.*;
+
+import java.util.Map;
+
+public class StatsCalculator {
+    static LayerStats calculateStats(AbstractLayer layer) {
+        Map<String, Double> maxNumeric = Maps.newConcurrentMap();
+        Map<String, Double> minNumeric = Maps.newConcurrentMap();
+
+        AttributeSchema attributeSchema = layer.schema();
+
+        layer.features().parallelStream()
+                .flatMap(feature -> feature.metadata().entrySet().stream())
+                .filter(entry -> attributeSchema.attributes().get(entry.getKey()).type()
+                        == AttributeType.NUMERIC)
+                .forEach(entry -> {
+                    Object value = entry.getValue();
+                    double doubleValue = Double.valueOf(value.toString());
+
+                    if (!maxNumeric.containsKey(entry.getKey())) {
+                        maxNumeric.put(entry.getKey(), doubleValue);
+                    } else if (doubleValue > maxNumeric.get(entry.getKey())) {
+                        maxNumeric.put(entry.getKey(), doubleValue);
+                    }
+                    if (!minNumeric.containsKey(entry.getKey())) {
+                        minNumeric.put(entry.getKey(), doubleValue);
+                    } else if (doubleValue < minNumeric.get(entry.getKey())) {
+                        minNumeric.put(entry.getKey(), doubleValue);
+                    }
+                });
+
+        ImmutableLayerStats.Builder builder = ImmutableLayerStats.builder();
+        layer.schema().attributes()
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().type() == AttributeType.NUMERIC)
+                .forEach(entry -> builder.putAttributeStats(entry.getKey(),
+                        ImmutableAttributeStats.builder()
+                                .minimum(minNumeric.get(entry.getKey()))
+                                .maximum(maxNumeric.get(entry.getKey()))
+                                .build()));
+
+        builder.featureCount(layer.features().size());
+
+        return builder.build();
+    }
+}
