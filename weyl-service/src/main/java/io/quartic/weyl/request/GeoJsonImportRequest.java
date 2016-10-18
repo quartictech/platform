95c5ce35f00b4ebcaf1d7cd24321350c0c533334
Alex Sparrow <alex.sparrow@cern.ch>
2016-10-18 06:28:35 -0400

    GeoJSON endpoint should include metadata

--- weyl-service/src/main/java/io/quartic/weyl/request/GeoJsonImportRequest.java
+++ weyl-service/src/main/java/io/quartic/weyl/request/GeoJsonImportRequest.java
@@ -1,13 +1,15 @@
 package io.quartic.weyl.request;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.quartic.weyl.core.geojson.FeatureCollection;
+import io.quartic.weyl.core.model.LayerMetadata;
 import org.immutables.value.Value;
 
 @Value.Immutable
 @JsonDeserialize(as=ImmutableGeoJsonImportRequest.class)
 public interface GeoJsonImportRequest {
-    String name();
-    String description();
+    @JsonUnwrapped
+    LayerMetadata metadata();
     FeatureCollection data();
 }
