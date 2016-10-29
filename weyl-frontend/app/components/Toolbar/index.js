bf7ea50d69e346ee6d502cb677937fcace401106
Alex Sparrow <alex@quartic.io>
2016-10-29 18:21:23 +0100

    Make toolbar floating, smaller and add logo

--- weyl-frontend/app/components/Toolbar/index.js
+++ weyl-frontend/app/components/Toolbar/index.js
@@ -7,6 +7,7 @@ import Search from "../Search";
 import classNames from "classnames";
 import { mapThemes } from "../../themes";
 const $ = require("jquery");
+import logo from "./quartic.svg";
 
 function Toolbar(props) {
   const bucketClassNames = classNames("item", { "active": props.ui.layerOp === "bucket" });
@@ -15,50 +16,59 @@ function Toolbar(props) {
   const liveFeedClassNames = classNames("item", { "active": props.ui.panels.liveFeed });
   const chartClassNames = classNames("item", { "active": props.ui.panels.chart });
   const themeClassNames = classNames("item");
+
+  const itemStyle = { "padding-top": "7px", "padding-bottom": "0px" };
+
   return (
     <div className={styles.toolbar}>
-      <div className="ui menu inverted attached labeled icon">
-        <div className="header item">
-          <i
-            className="icon map"
-            data-content={`Version: ${(process.env.BUILD_VERSION || "unknown")}`}
-            data-variation="mini"
-            ref={x => $(x).popup()}
-          />
-          <span className={styles.brand}>Quartic</span>
-        </div>
+      <div className="ui raised fluid segment inverted" style={{ padding: "5px" }}>
+        <div className="ui small menu compact inverted labeled icon">
+          <div className="header item" style={itemStyle}>
 
-        <div className="right menu">
-          <a className={themeClassNames} onClick={() => props.onUiToggle("theme")}>
-            <i className={`icon ${mapThemes[props.ui.settings.theme].icon}`}></i>
-            {mapThemes[props.ui.settings.theme].label}
-          </a>
-          <a className={layerListClassNames} onClick={() => props.onUiToggle("layerList")}>
-            <i className="icon list"></i>
-            Layers
-          </a>
-          <a className={bucketClassNames} onClick={() => props.onUiToggle("bucket")}>
-            <i className="icon object group"></i>
-            Bucket
-          </a>
-          <a className={geofenceClassNames} onClick={() => props.onUiToggle("geofence")}>
-            <i className="icon crop"></i>
-            Geofence
-          </a>
-          <a className={chartClassNames} onClick={() => props.onUiToggle("chart")}>
-            <i className="icon area chart"></i>
-            Chart
-          </a>
-          <a className={liveFeedClassNames} onClick={() => props.onUiToggle("liveFeed")}>
-            <i className="icon feed"></i>
-            Live Feed
-          </a>
-          <div className="item">
-            <Search
-              onSearch={props.onSearch}
-              onSelectLayer={props.onSelectLayer}
-              onSelectPlace={props.onSelectPlace}
-            />
+            <img
+              className={styles.logo}
+              src={logo}
+              role="presentation"
+              data-content={`Version: ${(process.env.BUILD_VERSION || "unknown")}`}
+              data-variation="mini"
+              ref={x => $(x).popup()}
+            >
+            </img>
+            <span className={styles.brand}>Quartic</span>
+          </div>
+
+          <div className="right menu">
+            <a className={themeClassNames} onClick={() => props.onUiToggle("theme")} style={itemStyle}>
+              <i className={`icon ${mapThemes[props.ui.settings.theme].icon}`}></i>
+              {mapThemes[props.ui.settings.theme].label}
+            </a>
+            <a className={layerListClassNames} onClick={() => props.onUiToggle("layerList")} style={itemStyle}>
+              <i className="icon list"></i>
+              Layers
+            </a>
+            <a className={bucketClassNames} onClick={() => props.onUiToggle("bucket")} style={itemStyle}>
+              <i className="icon object group"></i>
+              Bucket
+            </a>
+            <a className={geofenceClassNames} onClick={() => props.onUiToggle("geofence")} style={itemStyle}>
+              <i className="icon crop"></i>
+              Geofence
+            </a>
+            <a className={chartClassNames} onClick={() => props.onUiToggle("chart")} style={itemStyle}>
+              <i className="icon area chart"></i>
+              Chart
+            </a>
+            <a className={liveFeedClassNames} onClick={() => props.onUiToggle("liveFeed")} style={itemStyle}>
+              <i className="icon feed"></i>
+              Live Feed
+            </a>
+            <div className="item" style={itemStyle}>
+              <Search
+                onSearch={props.onSearch}
+                onSelectLayer={props.onSelectLayer}
+                onSelectPlace={props.onSelectPlace}
+              />
+            </div>
           </div>
         </div>
       </div>
