7e0a3e217abcc5fb57520317e1fc2778735ba508
Oliver Charlesworth <oliver@quartic.io>
2016-10-30 19:00:00 +0000

    Modify test-cases to prove that feedEvents are being handled

--- weyl-core/src/test/java/io/quartic/weyl/core/LayerStoreShould.java
+++ weyl-core/src/test/java/io/quartic/weyl/core/LayerStoreShould.java
@@ -6,9 +6,7 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 import io.quartic.weyl.common.uid.SequenceUidGenerator;
 import io.quartic.weyl.common.uid.UidGenerator;
 import io.quartic.weyl.core.feature.FeatureStore;
-import io.quartic.weyl.core.live.LayerState;
-import io.quartic.weyl.core.live.LayerStoreListener;
-import io.quartic.weyl.core.live.LayerSubscription;
+import io.quartic.weyl.core.live.*;
 import io.quartic.weyl.core.model.*;
 import io.quartic.weyl.core.source.SourceUpdate;
 import org.junit.Test;
@@ -17,7 +15,9 @@ import rx.Observable;
 import rx.Subscriber;
 import rx.subjects.PublishSubject;
 
+import java.time.Instant;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -25,6 +25,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static io.quartic.weyl.core.live.LayerView.IDENTITY_VIEW;
 import static io.quartic.weyl.core.model.AttributeType.NUMERIC;
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -32,12 +33,13 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.*;
 
 public class LayerStoreShould {
+    public static final Instant INSTANT = Instant.now();
+    public static final LayerId LAYER_ID = LayerId.of("666");
     private final UidGenerator<FeatureId> fidGenerator = SequenceUidGenerator.of(FeatureId::of);
     private final UidGenerator<LayerId> lidGenerator = SequenceUidGenerator.of(LayerId::of);
     private final FeatureStore featureStore = new FeatureStore(fidGenerator);
     private final LayerStore store = new LayerStore(featureStore, lidGenerator);
     private final GeometryFactory factory = new GeometryFactory();
-    public static final LayerId LAYER_ID = LayerId.of("666");
 
     @Test
     public void list_created_layers() throws Exception {
@@ -69,33 +71,37 @@ public class LayerStoreShould {
     }
 
     @Test
-    public void add_observed_features_to_layer() throws Exception {
+    public void add_observed_features_and_events_to_layer() throws Exception {
         final Subscriber<SourceUpdate> sub = createLayer(LAYER_ID);
 
         Observable.just(
-                updateFor(feature("a", "1")),
-                updateFor(feature("b", "2"))
+                updateFor(newArrayList(feature("a", "1")), newArrayList(event("789"))),
+                updateFor(newArrayList(feature("b", "2")), newArrayList(event("987")))
         ).subscribe(sub);
 
         final AbstractLayer layer = store.getLayer(LAYER_ID).get();
         assertThat(layer.features(),
                 containsInAnyOrder(feature("a", "1"), feature("b", "2")));
+        assertThat(layer.feedEvents(),
+                containsInAnyOrder(event("789"), event("987")));
     }
 
     @Test
-    public void notify_subscribers_of_features_added_to_layer() throws Exception {
+    public void notify_subscribers_of_observed_features_and_events() throws Exception {
         final Subscriber<SourceUpdate> sub = createLayer(LAYER_ID);
 
         Consumer<LayerState> subscriber = mock(Consumer.class);
         store.addSubscriber(LAYER_ID, subscriber);
 
         Observable.just(
-                updateFor(feature("a", "1"))
+                updateFor(newArrayList(feature("a", "1")), newArrayList(event("789")))
         ).subscribe(sub);
 
         final LayerState layerState = captureLiveLayerState(subscriber);
         assertThat(layerState.featureCollection(),
                 containsInAnyOrder(feature("a", "1")));
+        assertThat(layerState.feedEvents(),
+                containsInAnyOrder(event("789")));
         assertThat(layerState.schema(),
                 equalTo(ImmutableAttributeSchema.builder()
                         .attributes(ImmutableMap.of("timestamp", Attribute.of(NUMERIC, Optional.empty())))
@@ -187,7 +193,11 @@ public class LayerStoreShould {
     }
 
     private SourceUpdate updateFor(Feature... features) {
-        return SourceUpdate.of(asList(features), newArrayList());
+        return updateFor(asList(features), newArrayList());
+    }
+
+    private SourceUpdate updateFor(List<Feature> features, List<EnrichedFeedEvent> events) {
+        return SourceUpdate.of(features, events);
     }
 
     private Feature feature(String externalId, String uid) {
@@ -196,6 +206,14 @@ public class LayerStoreShould {
                 .uid(FeatureId.of(uid))
                 .geometry(factory.createPoint(new Coordinate(123.0, 456.0)))
                 .metadata(ImmutableMap.of("timestamp", 1234))
+                .build();
+    }
+
+    private EnrichedFeedEvent event(String id) {
+        return EnrichedFeedEvent.builder()
+                .id(LiveEventId.of(id))
+                .feedEvent(FeedEvent.of("foo", "bar", emptyMap()))
+                .timestamp(INSTANT)
                 .build();
     }
 
