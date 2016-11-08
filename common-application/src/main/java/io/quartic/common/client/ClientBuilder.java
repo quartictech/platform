e52975c57c7a3200fd1a69a1c7cbf544debfad89
Oliver Charlesworth <oliver@quartic.io>
2016-11-08 18:47:45 +0000

    Add Java version + strip "Application"

--- common-application/src/main/java/io/quartic/common/client/ClientBuilder.java
+++ common-application/src/main/java/io/quartic/common/client/ClientBuilder.java
@@ -30,7 +30,14 @@ public final class ClientBuilder {
 
     public static String userAgentFor(Class<?> clazz) {
         final String version = clazz.getPackage().getImplementationVersion();
-        return clazz.getSimpleName() + "/" + ((version == null) ? "unknown" : version);
+        return strippedName(clazz)
+                + "/" + ((version == null) ? "unknown" : version)
+                + " (Java " + System.getProperty("java.version") + ")";
+    }
+
+    private static String strippedName(Class<?> clazz) {
+        final String name = clazz.getSimpleName();
+        return name.replaceAll("Application$", "");
     }
 
 }
