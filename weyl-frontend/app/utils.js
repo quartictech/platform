8f95d35f0fc4337390c5446ae4737af61b2aab3d
Oliver Charlesworth <oliver@quartic.io>
2016-11-11 11:41:32 +0000

    Use WSS when protocol is HTTPS

--- weyl-frontend/app/utils.js
+++ weyl-frontend/app/utils.js
@@ -1,3 +1,3 @@
 export const mapboxToken = "pk.eyJ1IjoiYWxzcGFyIiwiYSI6ImNpcXhybzVnZTAwNTBpNW5uaXAzbThmeWEifQ.s_Z4AWim5WwKa0adU9P2Uw";
 export const apiRootUrl = `${location.origin}${location.pathname}api`;
-export const wsUrl = `ws://${location.host}${location.pathname}ws`;
+export const wsUrl = `${location.protocol == "https" ? "wss:" : "ws:"}//${location.host}${location.pathname}ws`;
