24ac800ce76061db4a6063ec20284f4ccea61736
Oliver Charlesworth <oliver@quartic.io>
2016-11-15 12:16:27 +0000

    Delete @PathParam from implementation methods (which appears to bork Jersey)

--- catalogue-application/src/main/java/io/quartic/catalogue/CatalogueResource.java
+++ catalogue-application/src/main/java/io/quartic/catalogue/CatalogueResource.java
@@ -17,7 +17,6 @@ import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.PathParam;
 import java.util.Map;
 import java.util.Set;
 
@@ -42,7 +41,7 @@ public class CatalogueResource extends Endpoint implements CatalogueService {
         return id;
     }
 
-    public synchronized void deleteDataset(@PathParam("id") String id) {
+    public synchronized void deleteDataset(String id) {
         final DatasetId did = DatasetId.of(id);
         throwIfDatasetNotFound(did);
         datasets.remove(did);
@@ -53,7 +52,7 @@ public class CatalogueResource extends Endpoint implements CatalogueService {
         return ImmutableMap.copyOf(datasets);
     }
 
-    public synchronized DatasetConfig getDataset(@PathParam("id") String id) {
+    public synchronized DatasetConfig getDataset(String id) {
         final DatasetId did = DatasetId.of(id);
         throwIfDatasetNotFound(did);
         return datasets.get(did);
