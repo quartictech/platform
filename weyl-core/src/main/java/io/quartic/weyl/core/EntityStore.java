1f41ba630eef009d831c05864d20aeeaba6e65c2
Oliver Charlesworth <oliver@quartic.io>
2016-11-18 11:35:53 +0000

    Add EntityStore.getObservable (+ tests)

--- weyl-core/src/main/java/io/quartic/weyl/core/EntityStore.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/EntityStore.java
@@ -2,20 +2,40 @@ package io.quartic.weyl.core;
 
 import io.quartic.weyl.core.model.AbstractFeature;
 import io.quartic.weyl.core.model.EntityId;
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
 
 import java.util.Collection;
 import java.util.Map;
 
 import static com.google.common.collect.Maps.newConcurrentMap;
+import static com.google.common.collect.Maps.newHashMap;
 
 public class EntityStore {
+    private final Map<EntityId, BehaviorSubject<AbstractFeature>> observables = newHashMap();
     private final Map<EntityId, AbstractFeature> attributes = newConcurrentMap();
 
     public void putAll(Collection<AbstractFeature> features) {
-        features.forEach(f -> attributes.put(f.entityId(), f));
+        features.forEach(f -> {
+            attributes.put(f.entityId(), f);
+            getSubject(f.entityId()).onNext(f);
+        });
     }
 
     public AbstractFeature get(EntityId id) {
         return attributes.get(id);
+    }
+
+    public Observable<AbstractFeature> getObservable(EntityId id) {
+        return getSubject(id);
+    }
+
+    private synchronized BehaviorSubject<AbstractFeature> getSubject(EntityId id) {
+        BehaviorSubject<AbstractFeature> subject = observables.get(id);
+        if (subject == null) {
+            subject = BehaviorSubject.create();
+            observables.put(id, subject);
+        }
+        return subject;
     }
 }
---
+++ weyl-core/src/test/java/io/quartic/weyl/core/EntityStoreShould.java
@@ -0,0 +1,97 @@
+package io.quartic.weyl.core;
+
+import io.quartic.weyl.core.model.AbstractFeature;
+import io.quartic.weyl.core.model.EntityId;
+import org.junit.Test;
+import rx.observers.TestSubscriber;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class EntityStoreShould {
+
+    private final EntityStore store = new EntityStore();
+
+    @Test
+    public void get_entities_the_legacy_way() throws Exception {
+        final EntityId idA = EntityId.of("123");
+        final EntityId idB = EntityId.of("456");
+        final AbstractFeature featureA = feature(idA);
+        final AbstractFeature featureB = feature(idB);
+
+        store.putAll(newArrayList(featureA, featureB));
+
+        assertThat(store.get(idA), equalTo(featureA));
+        assertThat(store.get(idB), equalTo(featureB));
+    }
+
+    @Test
+    public void get_most_recent_update_to_entity_the_legacy_way() throws Exception {
+        final EntityId id = EntityId.of("123");
+        final AbstractFeature featureA = feature(id);
+        final AbstractFeature featureB = feature(id);
+
+        store.putAll(newArrayList(featureA, featureB));
+
+        assertThat(store.get(id), equalTo(featureB));
+    }
+
+    @Test
+    public void emit_entity_changes() throws Exception {
+        final EntityId id = EntityId.of("123");
+        final AbstractFeature featureA = feature(id);
+        final AbstractFeature featureB = feature(id);
+        final TestSubscriber<AbstractFeature> sub = TestSubscriber.create();
+
+        store.getObservable(id).subscribe(sub);
+        store.putAll(newArrayList(featureA));
+        store.putAll(newArrayList(featureB));
+        sub.awaitValueCount(2, 100, MILLISECONDS);
+
+        assertThat(sub.getOnNextEvents(), contains(featureA, featureB));
+    }
+
+    @Test
+    public void emit_entities_for_different_ids() throws Exception {
+        final EntityId idA = EntityId.of("123");
+        final EntityId idB = EntityId.of("456");
+        final AbstractFeature featureA = feature(idA);
+        final AbstractFeature featureB = feature(idB);
+        final TestSubscriber<AbstractFeature> subA = TestSubscriber.create();
+        final TestSubscriber<AbstractFeature> subB = TestSubscriber.create();
+
+        store.getObservable(idA).subscribe(subA);
+        store.getObservable(idB).subscribe(subB);
+        store.putAll(newArrayList(featureA));
+        store.putAll(newArrayList(featureB));
+        subA.awaitValueCount(1, 100, MILLISECONDS);
+        subB.awaitValueCount(1, 100, MILLISECONDS);
+
+        assertThat(subA.getOnNextEvents(), contains(featureA));
+        assertThat(subB.getOnNextEvents(), contains(featureB));
+    }
+
+    @Test
+    public void emit_latest_entity_changes_on_subscription() throws Exception {
+        final EntityId id = EntityId.of("123");
+        final AbstractFeature feature = feature(id);
+        final TestSubscriber<AbstractFeature> sub = TestSubscriber.create();
+
+        store.putAll(newArrayList(feature));
+        store.getObservable(id).subscribe(sub);
+        sub.awaitValueCount(1, 100, MILLISECONDS);
+
+        assertThat(sub.getOnNextEvents(), contains(feature));
+    }
+
+    private AbstractFeature feature(EntityId id) {
+        final AbstractFeature feature = mock(AbstractFeature.class);
+        when(feature.entityId()).thenReturn(id);
+        return feature;
+    }
+}
