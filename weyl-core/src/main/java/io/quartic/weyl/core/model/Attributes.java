ba76558d80350188b834b262bc921fc000e2a11c
Oliver Charlesworth <oliver@quartic.io>
2016-11-21 22:38:45 +0000

    Revert bad change

--- weyl-core/src/main/java/io/quartic/weyl/core/model/Attributes.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/model/Attributes.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import io.quartic.common.SweetStyle;
 import org.immutables.value.Value;
 
-import java.util.Collections;
 import java.util.Map;
 
 @SweetStyle
 @Value.Immutable
 public interface Attributes {
-    Attributes EMPTY_ATTRIBUTES = Collections::emptyMap;
+    Attributes EMPTY_ATTRIBUTES = AttributesImpl.builder().build();
 
     @JsonValue
     Map<AttributeName, Object> attributes();
