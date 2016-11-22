dfa870aaaff4eeb3936b3390bc4da3eb86b0d04b
Oliver Charlesworth <oliver@quartic.io>
2016-11-22 00:42:54 +0000

    WIP

--- weyl-core/src/main/java/io/quartic/weyl/core/model/Attributes.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/model/Attributes.java
@@ -1,14 +1,11 @@
 package io.quartic.weyl.core.model;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import io.quartic.common.SweetStyle;
-import org.immutables.value.Value;
 
 import java.util.Collections;
 import java.util.Map;
 
-@SweetStyle
-@Value.Immutable
+// Not Immutable - we need people to use AttributesFactory
 public interface Attributes {
     Attributes EMPTY_ATTRIBUTES = Collections::emptyMap;
 
