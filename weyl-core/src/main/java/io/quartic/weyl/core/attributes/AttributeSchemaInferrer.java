86555f92acc1ddee7c717c447101a5e0d3ca1161
Oliver Charlesworth <oliver@quartic.io>
2016-11-24 17:53:13 +0000

    Add proper test-cases for AttributeSchemaInferrer

--- weyl-core/src/main/java/io/quartic/weyl/core/attributes/AttributeSchemaInferrer.java
+++ weyl-core/src/main/java/io/quartic/weyl/core/attributes/AttributeSchemaInferrer.java
@@ -3,32 +3,30 @@ package io.quartic.weyl.core.attributes;
 import io.quartic.weyl.core.model.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyMap;
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toConcurrentMap;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 public class AttributeSchemaInferrer {
+
+    public static final int MAX_CATEGORIES = 19;
+
     public static Map<AttributeName, Attribute> inferSchema(Collection<Feature> features) {
         final List<Attributes> attributes = features.stream().map(Feature::attributes).collect(toList());
-
         if (attributes.isEmpty()) {
             return emptyMap();
         }
 
         final Collection<AttributeName> names = attributes.iterator().next().attributes().keySet();   // They should all be the same
-
         return names.parallelStream()
                 .collect(toConcurrentMap(identity(), attribute -> inferAttribute(attribute, attributes)));
     }
 
     private static Attribute inferAttribute(AttributeName name, Collection<Attributes> attributes) {
-        Optional<Set<Object>> categories = inferCategories(name, attributes);
         return AttributeImpl.builder()
                 .type(inferAttributeType(name, attributes))
-                .categories(categories)
+                .categories(inferCategories(name, attributes))
                 .build();
     }
 
@@ -36,23 +34,21 @@ public class AttributeSchemaInferrer {
         Set<Object> values = attributes.stream()
                 .map(a -> a.attributes().get(name))
                 .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
+                .collect(toSet());
 
-        if (values.size() > 0 && values.size() < 20 && values.size() < attributes.size()) {
+        if (values.size() <= MAX_CATEGORIES && values.size() < attributes.size()) {
             return Optional.of(values);
-        }
-        else {
+        } else {
             return Optional.empty();
         }
     }
 
-    private static AttributeType inferAttributeType(AttributeName attribute,
-                                                    Collection<Attributes> attributes) {
+    private static AttributeType inferAttributeType(AttributeName attribute, Collection<Attributes> attributes) {
         Set<AttributeType> attributeTypes = attributes.stream()
                 .map(a -> a.attributes().get(attribute))
                 .filter(Objects::nonNull)
                 .map(AttributeSchemaInferrer::inferValueType)
-                .collect(Collectors.toSet());
+                .collect(toSet());
 
         if (attributeTypes.size() == 1) {
             return attributeTypes.iterator().next();
--- weyl-core/src/test/java/io/quartic/weyl/core/attributes/AttributeSchemaInferrerShould.java
+++ weyl-core/src/test/java/io/quartic/weyl/core/attributes/AttributeSchemaInferrerShould.java
@@ -1,39 +1,105 @@
 package io.quartic.weyl.core.attributes;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.vividsolutions.jts.geom.Geometry;
 import io.quartic.weyl.core.model.*;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static io.quartic.common.CollectionUtils.entry;
+import static io.quartic.common.CollectionUtils.map;
+import static io.quartic.weyl.core.attributes.AttributeSchemaInferrer.MAX_CATEGORIES;
 import static io.quartic.weyl.core.attributes.AttributeSchemaInferrer.inferSchema;
-import static io.quartic.weyl.core.model.AttributeType.NUMERIC;
+import static io.quartic.weyl.core.model.AttributeType.*;
+import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class AttributeSchemaInferrerShould {
+
+    @Test
+    public void infer_numeric_type() throws Exception {
+        assertThatTypeInferredFromValues(NUMERIC, 123, 456);
+    }
+
+    @Test
+    public void infer_numeric_type_when_stringified() throws Exception {
+        assertThatTypeInferredFromValues(NUMERIC, "123", "456");
+    }
+
+    @Test
+    public void infer_string_type() throws Exception {
+        assertThatTypeInferredFromValues(STRING, "foo", "bar");
+    }
+
+    @Test
+    public void infer_timeseries_type() throws Exception {
+        assertThatTypeInferredFromValues(TIME_SERIES, mock(TimeSeriesAttribute.class));
+    }
+
+    @Test
+    public void infer_unknown_type_when_mixed() throws Exception {
+        assertThatTypeInferredFromValues(UNKNOWN, "foo", 123);
+    }
+
+    private void assertThatTypeInferredFromValues(AttributeType type, Object... values) {
+        List<Feature> features = features("a", values);
+
+        assertThat(inferSchema(features), equalTo(map(entry(name("a"), AttributeImpl.of(type, Optional.empty())))));
+    }
+
+    @Test
+    public void infer_categories() throws Exception {
+        List<Feature> features = features("a", "foo", "bar", "foo");
+
+        assertThat(inferSchema(features), equalTo(map(
+                entry(name("a"), AttributeImpl.of(STRING, Optional.of(newHashSet("foo", "bar"))))
+        )));
+    }
+
+    @Test
+    public void infer_no_categories_when_all_distinct() throws Exception {
+        List<Feature> features = features("a", "foo", "bar", "baz");
+
+        assertThat(inferSchema(features), equalTo(map(
+                entry(name("a"), AttributeImpl.of(STRING, Optional.empty()))
+        )));
+    }
+
+    @Test
+    public void infer_no_categories_when_too_many() throws Exception {
+        String[] values = new String[MAX_CATEGORIES + 2];
+        for (int i = 0; i < MAX_CATEGORIES + 1; i++) {
+            values[i] = "foo" + i;
+        }
+        values[MAX_CATEGORIES + 1] = "foo0";    // To avoid the all-distinct check
+
+        List<Feature> features = features("a", values);
+
+        assertThat(inferSchema(features), equalTo(map(
+                entry(name("a"), AttributeImpl.of(STRING, Optional.empty()))
+        )));
+    }
+
     @Test
     public void ignore_missing_attributes() throws Exception {
-        List<Feature> features = Lists.newArrayList(
-                feature(ImmutableMap.of("a", 123, "b", 456)),
-                feature(ImmutableMap.of("a", 789))              // b is missing here
+        List<Feature> features = newArrayList(
+                feature(map(entry(name("a"), 123), entry(name("b"), 456))),
+                feature(map(entry(name("a"), 789), entry(name("b"), null)))
         );
 
-        assertThat(inferSchema(features),
-                equalTo(ImmutableMap.of(
-                        name("a"), AttributeImpl.of(NUMERIC, Optional.empty()),
-                        name("b"), AttributeImpl.of(NUMERIC, Optional.of(ImmutableSet.of(456)))
-                )));
+        assertThat(inferSchema(features), equalTo(map(
+                entry(name("a"), AttributeImpl.of(NUMERIC, Optional.empty())),
+                entry(name("b"), AttributeImpl.of(NUMERIC, Optional.of(newHashSet(456))))
+        )));
     }
 
     @Test
@@ -41,11 +107,17 @@ public class AttributeSchemaInferrerShould {
         assertThat(inferSchema(emptyList()).entrySet(), empty());
     }
 
-    private Feature feature(Map<String, ?> attributes) {
+    private List<Feature> features(String name, Object... values) {
+        return stream(values)
+                .map(v -> feature(map(entry(name(name), v))))
+                .collect(toList());
+    }
+
+    private Feature feature(Map<AttributeName, Object> attributes) {
         return FeatureImpl.builder()
                 .entityId(EntityIdImpl.of("xyz"))
                 .geometry(mock(Geometry.class))
-                .attributes(() -> attributes.entrySet().stream().collect(toMap(e -> name(e.getKey()), Entry::getValue)))
+                .attributes(() -> attributes)
                 .build();
     }
 
