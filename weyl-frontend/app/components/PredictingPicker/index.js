4e2525f6dc8536ac2a1a8af80c321b667343fef3
Oliver Charlesworth <oliver@quartic.io>
2016-12-06 18:02:19 +0000

    Fluff the linter

--- weyl-frontend/app/components/PredictingPicker/index.js
+++ weyl-frontend/app/components/PredictingPicker/index.js
@@ -34,6 +34,9 @@ class PredictingPicker extends React.Component { // eslint-disable-line react/pr
   }
 
   render() {
+    // Hack because pt-input overrides font-family and no way to pass styles through InputGroup
+    const fontSetter = (e) => e && (e.style.fontFamily = "Roboto-Light"); // eslint-disable-line no-param-reassign
+
     return (
       <Popover
         autoFocus={false}
@@ -52,7 +55,7 @@ class PredictingPicker extends React.Component { // eslint-disable-line react/pr
           value={this.state.text}
           onChange={(e) => this.onChangeText(e.target.value)}
           intent={(this.props.selectedKey || this.props.disabled) ? Intent.NONE : Intent.DANGER}
-          inputRef={(e) => e && (e.style.fontFamily = "Roboto-Light")}  // Hack because pt-input overrides font-family and no way to pass styles through InputGroup
+          inputRef={fontSetter}
         />
       </Popover>
     );
--- weyl-frontend/app/components/Search/index.js
+++ weyl-frontend/app/components/Search/index.js
@@ -30,6 +30,9 @@ class Search extends React.Component {
       <Menu>{this.renderMenuItems()}</Menu>
     );
 
+    // Hack because pt-input overrides font-family and no way to pass styles through InputGroup
+    const fontSetter = (e) => e && (e.style.fontFamily = "Roboto-Light"); // eslint-disable-line no-param-reassign
+
     return (
       <Popover
         autoFocus={false}
@@ -47,7 +50,7 @@ class Search extends React.Component {
           placeholder="Search datasets..."
           value={this.state.query}
           onChange={(e) => this.setQuery(e.target.value)}
-          inputRef={(e) => e && (e.style.fontFamily = "Roboto-Light")}  // Hack because pt-input overrides font-family and no way to pass styles through InputGroup
+          inputRef={fontSetter}
         />
       </Popover>
     );
