2340a365d947eabeeebad22d08f6d43e0ff13dfb
Alex Sparrow <alex@quartic.io>
2016-12-07 12:40:37 +0000

    Vector tile resource should 204 on empty tiles
    Currently the library we're using returns an empty byte array when there are no features in a tile. Detect this case in the resource method and explicitly return null (204).

--- weyl-application/src/main/java/io/quartic/weyl/resource/TileResource.java
+++ weyl-application/src/main/java/io/quartic/weyl/resource/TileResource.java
@@ -27,13 +27,18 @@ public class TileResource {
     @Path("/{layerId}/{z}/{x}/{y}.pbf")
     @CacheControl(maxAge = 60*60)
     public byte[] protobuf(@PathParam("layerId") LayerId layerId,
-                           @PathParam("z") Integer z,
-                           @PathParam("x") Integer x,
-                           @PathParam("y") Integer y) throws ParseException, IOException {
+                             @PathParam("z") Integer z,
+                             @PathParam("x") Integer x,
+                             @PathParam("y") Integer y) throws ParseException, IOException {
         Layer layer = layerStore.getLayer(layerId)
                 .orElseThrow(() -> new NotFoundException("No layer with id: " + layerId));
 
-        return new VectorTileRenderer(ImmutableList.of(layer))
+        byte[] data = new VectorTileRenderer(ImmutableList.of(layer))
                 .render(z, x, y);
+        if (data.length == 0) {
+            return null;
+        }
+
+        return data;
     }
 }
