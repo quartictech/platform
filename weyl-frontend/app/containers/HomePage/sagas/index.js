e29f8989f46ba8e2b87dac44a7c7feea442f75d0
Oliver Charlesworth <oliver@quartic.io>
2016-12-11 21:53:03 +0000

    Fix up front-end

--- weyl-frontend/app/containers/HomePage/sagas/index.js
+++ weyl-frontend/app/containers/HomePage/sagas/index.js
@@ -1,39 +1,12 @@
-import { take, call, put, fork, cancel } from "redux-saga/effects";
+import { take, fork, cancel } from "redux-saga/effects";
 import { takeLatest } from "redux-saga";
 
 import { LOCATION_CHANGE } from "react-router-redux";
-import request from "utils/request";
 import * as constants from "../constants";
-import * as actions from "../actions";
-import { apiRootUrl } from "../../../utils.js";
 
 import search from "./search";
 import manageSocket from "./manageSocket";
-
-function* runLayerComputation(action) {
-  const requestURL = `${apiRootUrl}/layer/compute`;
-  const results = yield call(request, requestURL, {
-    method: "PUT",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(action.computation),
-  });
-
-  if (!results.err) {
-    const requestURL2 = `${apiRootUrl}/layer/metadata/${results.data}`;
-    const results2 = yield call(request, requestURL2, {
-      method: "GET",
-    });
-
-    if (!results2.err) {
-      yield put(actions.layerCreate(results2.data));
-    }
-  }
-}
-
-// ////////////////////////
+import performComputation from "./performComputation";
 
 function watch(action, generator) {
   return function* () {
@@ -52,5 +25,5 @@ function prepare(generator) {
 export default [
   prepare(manageSocket),
   prepare(watch(constants.SEARCH, search)),
-  prepare(watch(constants.LAYER_COMPUTATION_START, runLayerComputation)),
+  prepare(watch(constants.LAYER_COMPUTATION_START, performComputation)),
 ];
