abdee462f04bab981fa9611e07e16d3dd2f39ebe
Oliver Charlesworth <oliver@quartic.io>
2016-12-15 11:02:36 +0000

    Fix brokenness

--- weyl-frontend/app/app.js
+++ weyl-frontend/app/app.js
@@ -24,7 +24,6 @@ import configureStore from "./store";
 window.$ = window.jQuery = require("jquery");
 import "sanitize.css/sanitize.css";
 import "@blueprintjs/core/dist/blueprint.css";
-import "@blueprintjs/table/dist/table.css";
 import { FocusStyleManager } from "@blueprintjs/core";
 FocusStyleManager.onlyShowFocusOnTabs();      // To avoid annoying blue outlines
 import "roboto-fontface/css/roboto/roboto-fontface.css";
