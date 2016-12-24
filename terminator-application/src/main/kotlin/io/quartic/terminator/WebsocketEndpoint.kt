524c3e22db1e04667d4ec177901bd5cb148a5102
Oliver Charlesworth <oliver@quartic.io>
2016-12-24 20:02:43 +0000

    Convert more stuff and factor out common logging utility

---
+++ terminator-application/src/main/kotlin/io/quartic/terminator/WebsocketEndpoint.kt
@@ -0,0 +1,30 @@
+package io.quartic.terminator
+
+import com.codahale.metrics.annotation.ExceptionMetered
+import com.codahale.metrics.annotation.Metered
+import com.codahale.metrics.annotation.Timed
+import com.fasterxml.jackson.core.JsonProcessingException
+import io.quartic.common.logging.logger
+import io.quartic.common.serdes.ObjectMappers.OBJECT_MAPPER
+import io.quartic.common.server.ResourceManagingEndpoint
+import io.quartic.terminator.api.FeatureCollectionWithTerminationId
+import rx.Observable
+import rx.Subscription
+import javax.websocket.Session
+
+@Metered
+@Timed
+@ExceptionMetered
+class WebsocketEndpoint(private val observable: Observable<FeatureCollectionWithTerminationId>) : ResourceManagingEndpoint<Subscription>() {
+    val LOG by logger()
+
+    override fun createResourceFor(session: Session) = observable.subscribe { fcwdi ->
+        try {
+            session.asyncRemote.sendText(OBJECT_MAPPER.writeValueAsString(fcwdi))
+        } catch (e: JsonProcessingException) {
+            LOG.error("Error producing JSON", e)
+        }
+    }
+
+    override fun releaseResource(subscription: Subscription) = subscription.unsubscribe()
+}
