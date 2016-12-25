dcb993c6b20a535af1cc59ec02a7e9152e01aa2f
Oliver Charlesworth <oliver@quartic.io>
2016-12-25 12:04:30 +0000

    Port InputStreamEncoder

--- common-core/src/main/kotlin/io/quartic/common/client/ClientUtils.kt
+++ common-core/src/main/kotlin/io/quartic/common/client/ClientUtils.kt
@@ -4,12 +4,19 @@ import com.google.common.net.HttpHeaders.USER_AGENT
 import feign.Feign
 import feign.Logger.Level.BASIC
 import feign.Retryer
+import feign.codec.EncodeException
+import feign.codec.Encoder
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.jaxrs.JAXRSContract
 import feign.slf4j.Slf4jLogger
 import io.quartic.common.ApplicationDetails
 import io.quartic.common.serdes.objectMapper
+import org.apache.commons.io.IOUtils.copy
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
 
 
 inline fun <reified T : Any> client(owner: Class<*>, url: String): T = client(T::class.java, owner, url)
@@ -17,13 +24,30 @@ inline fun <reified T : Any> client(owner: Class<*>, url: String): T = client(T:
 // TODO: eliminate overload once everything ported to Kotlin
 fun <T> client(target: Class<T>, owner: Class<*>, url: String): T = Feign.builder()
         .contract(JAXRSContract())
-        .encoder(InputStreamEncoder(JacksonEncoder(objectMapper())))
+        .encoder(inputStreamEncoder(JacksonEncoder(objectMapper())))
         .decoder(JacksonDecoder(objectMapper()))
         .retryer(Retryer.Default(0, 0, 1))
         .requestInterceptor { template -> template.header(USER_AGENT, userAgentFor(owner)) }
         .logger(Slf4jLogger())
         .logLevel(BASIC)
         .target(target, url)
+
+private fun inputStreamEncoder(delegate: Encoder) = Encoder { obj, bodyType, template ->
+    // Adapted from: http://www.monkeypatch.io/2016/08/10/MKTD-1-feign-vs-retrofit-&-58;-2-going-further.html
+    if (InputStream::class.java == bodyType) {
+        val inputStream = InputStream::class.java.cast(obj)
+        try {
+            ByteArrayOutputStream().use { bos ->
+                copy(inputStream, bos)
+                template.body(bos.toByteArray(), StandardCharsets.UTF_8)
+            }
+        } catch (e: IOException) {
+            throw EncodeException("Cannot upload file", e)
+        }
+    } else {
+        delegate.encode(obj, bodyType, template)
+    }
+}
 
 fun userAgentFor(clazz: Class<*>): String {
     val details = ApplicationDetails(clazz)
