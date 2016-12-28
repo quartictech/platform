390966f948bf6e9381ec3539c124c12f8e922dbf
Alex Sparrow <alex@quartic.io>
2016-12-28 09:12:04 +0000

    Remove intellij crud

--- catalogue-api/src/test/java/io/quartic/catalogue/api/WebsocketDatasetLocatorShould.java
+++ catalogue-api/src/test/java/io/quartic/catalogue/api/WebsocketDatasetLocatorShould.java
@@ -1,8 +1,5 @@
 package io.quartic.catalogue.api;
 
-/**
- * Created by alex on 28/12/16.
- */
 public class WebsocketDatasetLocatorShould extends DatasetLocatorTests<WebsocketDatasetLocator> {
     @Override
     protected WebsocketDatasetLocator locator() {
