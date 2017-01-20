f8fb355b307a922337233475a95d5b0437831b55
Oliver Charlesworth <oliver@quartic.io>
2017-01-20 19:07:21 +0000

    Terminate terminator

--- terminator-api/src/main/kotlin/io/quartic/terminator/api/FeatureCollectionWithTerminationId.kt
+++
@@ -1,9 +0,0 @@
-package io.quartic.terminator.api
-
-import io.quartic.catalogue.api.TerminationId
-import io.quartic.common.geojson.FeatureCollection
-
-data class FeatureCollectionWithTerminationId(
-    val terminationId: TerminationId,
-    val featureCollection: FeatureCollection
-)
