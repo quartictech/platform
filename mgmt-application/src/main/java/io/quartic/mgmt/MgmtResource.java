f02382633c9d06fc9be8d1eec3b5384ef4497103
Alex Sparrow <alex@quartic.io>
2017-02-14 17:08:05 +0000

    Allow upload of raw data

--- mgmt-application/src/main/java/io/quartic/mgmt/MgmtResource.java
+++ mgmt-application/src/main/java/io/quartic/mgmt/MgmtResource.java
@@ -102,6 +102,7 @@ public class MgmtResource {
                             }
                         });
                 return storageId.getUid();
+            case RAW:
             default:
                 return fileName;
         }
