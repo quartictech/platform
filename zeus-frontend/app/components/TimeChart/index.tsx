3a04cf118b75ca74d3adc88c00be15b99ac6c60f
Alex Sparrow <alex@quartic.io>
2017-03-10 16:32:30 +0000

    Add time series plot

--- zeus-frontend/app/components/TimeChart/index.tsx
+++ zeus-frontend/app/components/TimeChart/index.tsx
@@ -5,9 +5,28 @@ const s = require("./style.css");
 
 import SizeMe from "react-sizeme";
 
+interface ITimeSeriesPoint {
+  x: Date;
+  y: number;
+}
+
+function generateTimeSeries(startDate: Date, endDate): ITimeSeriesPoint[] {
+  const data = [];
+  for (var d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+    data.push({
+      x:new Date(d),
+      y: Math.random()
+  });
+
+}
+
+  return data;
+}
+
 class RealTimeChart  extends React.Component<any, any> {
-  maintenance = [{ x: new Date(2017, 1, 1) }, { x: new Date(2017, 2, 3) }];
-  failures = [{ x: new Date(2017, 4, 1) }];
+  maintenance = [{ x: new Date(2017, 0, 1) }, { x: new Date(2017, 1, 3) }];
+  failures = [{ x: new Date(2017, 2, 2) }];
+  timeSeries: ITimeSeriesPoint[] = generateTimeSeries(new Date(2016, 1, 1), Date.now());
 
   constructor() {
     super();
@@ -23,6 +42,8 @@ class RealTimeChart  extends React.Component<any, any> {
     colorScale.range(['#1F77B4', '#FF7F0E']);
     const gridLines = new Plottable.Components.Gridlines(xScale, null);
 
+    const yScaleTimeSeries = new Plottable.Scales.Linear();
+
     const plot = new Plottable.Plots.Segment()
       .addDataset(new Plottable.Dataset(this.maintenance).metadata("maintenance"))
       .addDataset(new Plottable.Dataset(this.failures).metadata("failure"))
@@ -31,10 +52,18 @@ class RealTimeChart  extends React.Component<any, any> {
        .y(function(_) { return 0; }, yScale)
        .x2(d => d.x)
        .y2( _ => 1);
-    const chart = new Plottable.Components.Group([plot, gridLines]);
+
+    const timeSeriesPlot = new Plottable.Plots.Line()
+      .addDataset(new Plottable.Dataset(this.timeSeries))
+      .attr("stroke", _ => "#D3D3D3")
+      .x(d => d.x, xScale)
+      .y(d => d.y, yScaleTimeSeries);
+
+    const chart = new Plottable.Components.Group([timeSeriesPlot, plot, gridLines]);
     let pzi = new Plottable.Interactions.PanZoom();
     pzi.addXScale(xScale);
     pzi.attachTo(plot);
+    pzi.attachTo(timeSeriesPlot);
     window.addEventListener("resize", function() {
       plot.redraw();
     });
