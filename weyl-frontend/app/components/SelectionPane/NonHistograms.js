620d9381f31e65e1699b33ac91ee27df806a5cb7
Alex Sparrow <alex@quartic.io>
2017-03-27 16:23:22 +0100

    Fluff

--- weyl-frontend/app/components/SelectionPane/NonHistograms.js
+++ weyl-frontend/app/components/SelectionPane/NonHistograms.js
@@ -85,10 +85,10 @@ const Image = ({ url }) => {
   );
 };
 
-const urlMatch = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+const urlMatch = /https?:.*?/gi;
 const AttributeValue = ({ value }) => (<small>
-  {typeof value === 'string' && value.match(urlMatch) ? <a href={value}>Link</a>: value}
-  </small>
+  {typeof value === "string" && value.match(urlMatch) ? <a href={value}>Link</a> : value}
+</small>
 );
 
 const AttributesTable = ({ featureAttributes, behavior, order, showHeaders }) => (
@@ -119,7 +119,7 @@ const AttributesTable = ({ featureAttributes, behavior, order, showHeaders }) =>
                     style={{ fontWeight: "bold", wordWrap: "break-word", backgroundColor: Colors.DARK_GRAY3 }}
                     key={id}
                   >
-                    <AttributeValue value={behavior.render(key, attrs[key])}/>
+                    <AttributeValue value={behavior.render(key, attrs[key])} />
                   </td>
                 ))
               }
