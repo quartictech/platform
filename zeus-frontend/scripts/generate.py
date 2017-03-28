ce329a3071546628743bffc7095240569a008dd8
Alex Sparrow <alex@quartic.io>
2017-03-28 11:48:09 +0100

    A load more tweaks to fake data

--- zeus-frontend/scripts/generate.py
+++ zeus-frontend/scripts/generate.py
@@ -2,6 +2,7 @@
 import random
 import json
 from datetime import timedelta, datetime, date
+from shapely.geometry import shape, Point
 
 def default(obj):
     """Default JSON serializer."""
@@ -63,11 +64,11 @@ LOCATIONS = [
 ]
 
 
-def generate_assets():
+def generate_assets(asset_coords):
   assets = {}
   for i in range(50):
     model = random.choice(MODELS)
-    location = random.choice(LOCATIONS)
+    location = random.choice(asset_coords)
     id = "AB" + str(random.randrange(90000) + 10000)
     assets[id] = {
       "id": id,
@@ -97,7 +98,51 @@ def generate_assets():
     }
   return assets
 
-def make_feature(asset):
+def generate_insights(assets):
+  insights = []
+  for i in range(10):
+    number_assets = 10
+    zero_to_five = random.randrange(number_assets - 2)
+    five_to_ten = random.randrange(number_assets - zero_to_five)
+    greater_than_ten = random.randrange(number_assets - five_to_ten - zero_to_five)
+    insights.append({
+      "id": str(100 + i),
+      "insightType": "cluster",
+      "title": "Similar failures occurring in asset class",
+      "subInsights": [
+      {"icon": "pt-icon-info-sign", "text":"Statistically significant increase in failure rate following maintenance work"},
+      {"icon": "pt-icon-info-sign", "text":"Baseline voltage of circuit diagnostic increased following maintenance activity"}
+    ],
+    "assetClass": "Signal",
+    "assetIds": random.sample(list(assets), 10),
+    "barChart": {
+      "data": [
+        { "name": "0 -5 years", "value": zero_to_five },
+        { "name": "5-10 years", "value": five_to_ten },
+        { "name": "> 10 years", "value": greater_than_ten } 
+      ],
+      "xLabel": "# Failures",
+      "yLabel": "Asset age"
+    }
+    })
+  for i in range(110, 120):
+    insights.append({
+     "id": str(i),
+     "insightType": "failure",
+     "title": "Asset failure predicted",
+     "assetIds": random.sample(list(assets), 1),
+     "assetClass": "Signal",
+     "subInsights": [
+       {"icon": "pt-icon-info-sign", "text": "An increase has been detected in the diagnostic voltage circuit"},
+       {"icon": "pt-icon-info-sign", "text": "In 35% of previous occurences, this led to a failure within 2 weeks"}
+     ],
+     "events":[]
+   })
+  random.shuffle(insights)
+  return insights
+
+
+def make_feature(insight, asset):
   return {
         "type": "Feature",
         "geometry": {
@@ -105,17 +150,28 @@ def make_feature(asset):
           "coordinates": [asset["location"]["lon"], asset["location"]["lat"]]
         },
         "properties": {
-          "id": asset["id"]
+          "id": asset["id"],
+          "View in Inbox": "http://localhost:3020/insights/{0}".format(insight["id"])
         }
   } 
 
 if __name__ == "__main__":
-  assets = generate_assets()
+  lines = json.load(open("data/linie.geojson"))
+  switzerland = shape(json.load(open("data/switzerland.geojson"))["features"][0]["geometry"])
+  asset_coords = [(f["geometry"]["coordinates"][0][0], f["geometry"]["coordinates"][0][1]) for f in lines["features"]]
+  asset_coords.extend( [(f["geometry"]["coordinates"][1][0], f["geometry"]["coordinates"][1][1]) for f in lines["features"]])
+
+  asset_coords = [coord for coord in asset_coords if switzerland.contains(Point(coord[0], coord[1]))]
+
+  assets = generate_assets(asset_coords)
   json.dump(assets, open("data/assets.json", "w"), default=default, indent=1)
 
-  insight_asset_ids = random.sample(list(assets), 4)
+  insights = generate_insights(assets)
+  json.dump(insights, open("data/insights.json", "w"), default=default, indent=1)
+
+  insight_asset_ids = random.sample(insights[0]["assetIds"], 4)
   geojson = {
     "type": "FeatureCollection",
-    "features": [make_feature(assets[asset_id]) for asset_id in insight_asset_ids]
+    "features": [make_feature(insights[0], assets[asset_id]) for asset_id in insight_asset_ids]
   }
   json.dump(geojson, open("data/failure-predictions.geojson", "w"), indent=1)
