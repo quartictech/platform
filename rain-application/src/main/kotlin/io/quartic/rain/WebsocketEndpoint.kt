f06aefc3e29cd29dfeba9d5cac0044d1f06b87f7
Oliver Charlesworth <oliver@quartic.io>
2017-05-03 09:43:44 +0100

    Eliminate Immutables from weyl-api

--- rain-application/src/main/kotlin/io/quartic/rain/WebsocketEndpoint.kt
+++ rain-application/src/main/kotlin/io/quartic/rain/WebsocketEndpoint.kt
@@ -2,8 +2,6 @@ package io.quartic.rain
 
 import com.fasterxml.jackson.core.JsonFactory
 import com.google.common.base.Stopwatch
-import io.quartic.common.geojson.Feature
-import io.quartic.common.geojson.FeatureCollection
 import io.quartic.common.geojson.GeoJsonGenerator
 import io.quartic.common.geojson.GeoJsonParser
 import io.quartic.common.serdes.OBJECT_MAPPER
@@ -14,16 +12,10 @@ import io.quartic.howl.api.HowlClient
 import io.quartic.howl.api.StorageBackendChange
 import io.quartic.howl.api.StorageBackendChangeImpl
 import io.quartic.weyl.api.LayerUpdateType
-import io.quartic.weyl.api.LiveEvent
-import io.quartic.weyl.api.LiveEventImpl
 import org.slf4j.LoggerFactory
 import rx.Subscription
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.time.Instant
 import java.util.*
 import java.util.concurrent.TimeUnit
-import java.util.stream.Collectors
 import java.util.stream.StreamSupport
 import javax.websocket.Session
 
