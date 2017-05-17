a7628385dafb88393a70c4d25701f1b1bfda5462
Alex Sparrow <alex@quartic.io>
2017-05-17 15:58:38 +0100

    LINT OC-PRIVILEGED

--- zeus-frontend/app/routes/index.tsx
+++ zeus-frontend/app/routes/index.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import { IndexRedirect, Route } from "react-router";
 import { App, Insights, AssetView, Inventory, InsightView } from "../containers";
 
-import { useRouterHistory } from 'react-router'
-import { createHashHistory } from 'history'
+import { useRouterHistory } from "react-router";
+import { createHashHistory } from "history";
 
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 function getRoutes() { return (
     <Route path="/" component={App}>
