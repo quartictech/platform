d7fe1d8f3b858c04f1ec402bbbf96385350c8978
Oliver Charlesworth <oliver@quartic.io>
2017-05-17 15:40:54 +0100

    Wire up to config

--- zeus-backend/src/main/kotlin/io/quartic/zeus/ZeusApplication.kt
+++ zeus-backend/src/main/kotlin/io/quartic/zeus/ZeusApplication.kt
@@ -4,20 +4,30 @@ import io.dropwizard.assets.AssetsBundle
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import io.quartic.common.application.ApplicationBase
+import io.quartic.common.logging.logger
 import io.quartic.zeus.model.DatasetName
 import io.quartic.zeus.provider.ClasspathDataProvider
 import io.quartic.zeus.resource.DatasetResource
 
 class ZeusApplication : ApplicationBase<ZeusConfiguration>() {
+    private val LOG by logger()
+
     public override fun initializeApplication(bootstrap: Bootstrap<ZeusConfiguration>) {
         bootstrap.addBundle(AssetsBundle("/assets", "/", "index.html"))
     }
 
     override fun runApplication(configuration: ZeusConfiguration, environment: Environment) {
-        environment.jersey().register(DatasetResource(mapOf(
-                DatasetName("assets") to ClasspathDataProvider("/assets.json")  // TODO: get rid of this long-term
-        )))
+        environment.jersey().register(DatasetResource(createDatasetMap(configuration.datasets)))
     }
+
+    private fun createDatasetMap(config: Map<DatasetName, DataProviderConfiguration>) = config
+            .onEach { LOG.info("Registered data provider: ${it.key} -> ${it.value}") }
+            .mapValues {
+                val v = it.value
+                when (v) {
+                    is ClasspathDataProviderConfiguration -> ClasspathDataProvider(v.resourceName)
+                }
+            }
 
     companion object {
         @JvmStatic fun main(args: Array<String>) = ZeusApplication().run(*args)
