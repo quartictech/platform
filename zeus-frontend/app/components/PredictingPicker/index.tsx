951dc244d42774d7bbdb14b56dd26458457a0fa9
Oliver Charlesworth <oliver@quartic.io>
2017-05-18 22:12:22 +0100

    WIP

--- zeus-frontend/app/components/PredictingPicker/index.tsx
+++ zeus-frontend/app/components/PredictingPicker/index.tsx
@@ -16,13 +16,14 @@ import {
 } from "@blueprintjs/core";
 import * as classNames from "classnames";
 import * as _ from "underscore";
+const s = require("./style.css");
 
 // TODO: sort out inverse text colouring when selected
 // TODO: sort out bold font weight
 // TODO: look into https://basarat.gitbooks.io/typescript/docs/types/index-signatures.html
 // TODO: add scrolling (see https://github.com/palantir/blueprint/pull/1049)
 // TODO: add back convenience support for array of string entries
-
+// TODO: spinner?
 
 interface PredictingPickerEntry {
   key: string;
@@ -58,9 +59,8 @@ interface PredictingPickerProps {
 interface PredictingPickerState {
   text: string;
   menuVisible: boolean;
-  shouldFilter: boolean;
   idxHighlighted: number;
-  mouseCaptured: boolean;
+  sortedEntries: CategorisedEntries;  // Cached to avoid recompute during render
 }
 
 export default class PredictingPicker extends React.Component<PredictingPickerProps, PredictingPickerState> {
@@ -74,9 +74,8 @@ export default class PredictingPicker extends React.Component<PredictingPickerPr
     this.state = {
       text: "",               // Text to be displayed in search box
       menuVisible: false,
-      shouldFilter: false,
       idxHighlighted: 0,
-      mouseCaptured: false,
+      sortedEntries: null,
     };
 
     // TODO: do we still need these in TS?
@@ -85,15 +84,111 @@ export default class PredictingPicker extends React.Component<PredictingPickerPr
     this.onChangeText = this.onChangeText.bind(this);
   }
 
+  public componentWillMount() {
+    this.recalculateSortedEntries(this.props.entries);
+  }
+
   public componentWillReceiveProps(nextProps: PredictingPickerProps) {
-    // The selection can be affected either by either (1) what the user types into the search box, or (2) by
-    // explicitly selecting something in the menu.  The mechanism below ensure the search-box text is updated in case
-    // #2 (assuming a feedback mechanism from onChange to selectedKey outside this component).
-    // However, in case #1 there's the case where the user types something that doesn't match any entry, leading to
-    // key being null.  Hence the logic that skips the update in that case.
-    if (nextProps.selectedKey && (this.props.selectedKey !== nextProps.selectedKey)) {
-      this.setState({ text: this.props.entries[nextProps.selectedKey].name });
+    // TODO: controlled selection
+
+    // // The selection can be affected either by either (1) what the user types into the search box, or (2) by
+    // // explicitly selecting something in the menu.  The mechanism below ensure the search-box text is updated in case
+    // // #2 (assuming a feedback mechanism from onChange to selectedKey outside this component).
+    // // However, in case #1 there's the case where the user types something that doesn't match any entry, leading to
+    // // key being null.  Hence the logic that skips the update in that case.
+    // if (nextProps.selectedKey && (this.props.selectedKey !== nextProps.selectedKey)) {
+    //   this.setState({ text: this.props.entries[nextProps.selectedKey].name });
+    // }
+
+    // Note: this is a shallow check
+    if (this.props.entries !== nextProps.entries) {
+      this.recalculateSortedEntries(nextProps.entries);
+      this.resetHighlight();
     }
+  }
+
+  private recalculateSortedEntries(entries: PredictingPickerEntry[]) {
+    let idx = 0;
+    // TODO: make types work properly
+    const sortedEntries: {} = _.chain(entries)
+      .groupBy(entry => entry.category)
+      .map((entries, category: string) => [category, _.map(entries, entry => ({idx: idx++, entry} as NumberedEntry))])
+      .object()
+      .value();
+    this.setState({ sortedEntries });
+  }
+
+  private onMouseEnter(idx: number) {
+    this.setHighlightIndex(idx);
+  }
+
+  private onKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    switch (e.which) {
+      case Keys.ARROW_DOWN:
+        this.setHighlightIndex(this.state.idxHighlighted + 1);
+        break;
+
+      case Keys.ARROW_UP:
+        this.setHighlightIndex(this.state.idxHighlighted - 1);
+        break;
+
+      case Keys.ENTER: {
+        const highlighted = this.getHighlightedEntry();
+        if (highlighted) {
+          this.onSelectEntry(highlighted.key);
+        }
+        break;
+      }
+
+      default:
+        return;
+    }
+
+    e.preventDefault();
+  }
+
+  private onSelectEntry(key: string) {
+    this.hideMenu();
+    this.props.onChange(key);
+  }
+
+  private onInteraction(nextOpenState: boolean) {
+    nextOpenState ? this.showMenu() : this.hideMenu();
+    this.resetHighlight();
+  }
+
+  private onChangeText(text: string) {
+    this.setState({ text });
+    this.showMenu();
+    this.resetHighlight();
+
+    const matchingEntry = _.find(this.props.entries, entry => stringInString(text, entry.name));
+    this.props.onChange(matchingEntry ? matchingEntry.key : undefined); // TODO: is this nice?
+  }
+
+  private resetHighlight() {
+    this.setHighlightIndex(0);
+  }
+
+  private setHighlightIndex(idx: number) {
+    const clamped = Math.min(_.size(this.props.entries) - 1, Math.max(0, idx));
+    this.setState({ idxHighlighted: clamped });
+  }
+
+  private showMenu() {
+    this.setState({ menuVisible: true });
+  }
+
+  private hideMenu() {
+    this.setState({ menuVisible: false });
+  }
+
+  private getHighlightedEntry() {
+    return _.chain(this.state.sortedEntries)
+          .values()
+          .flatten()
+          .find(entry => entry.idx === this.state.idxHighlighted)
+          .value();
   }
 
   public render() {
@@ -123,7 +218,7 @@ export default class PredictingPicker extends React.Component<PredictingPickerPr
   }
 
   private renderMenu() {
-    const items = _.map(this.categorisedFilteredEntries(), (entries, category: string) => this.renderCategory(category, entries));
+    const items = _.map(this.state.sortedEntries, (entries, category: string) => this.renderCategory(category, entries));
 
     return (
       <Menu>
@@ -139,22 +234,29 @@ export default class PredictingPicker extends React.Component<PredictingPickerPr
   private renderCategory(category: string, entries: NumberedEntry[]) {
     return (
       <div key={category}>
-        {(category !== "undefined") && <MenuDivider title={category} />}
+        {(category !== undefined) && <MenuDivider title={category} />}
         {_.map(entries, entry => this.renderEntry(entry.entry, entry.idx))}
       </div>
     );
   }
 
-  // 25px is a hack - compensates for hardcoded ::before size in Blueprint CSS
+  // 25px is a hack - compensates for hardcoded ::before size in Blueprint CSS.
+  // Note that the MenuItem behaviour is not very controllable, so we completely override it - we've disabled 
+  // pointer-events, and use a wrapper div to capture events and do colouring.
   private renderEntry(entry: PredictingPickerEntry, idx: number) {
+    const isHighlighted = (idx === this.state.idxHighlighted);
     return (
       <div
         key={entry.key}
-        style={(idx === this.state.idxHighlighted) ? { backgroundColor: Colors.BLUE3 } : {}}  // TODO: set ::before color to white
+        style={{
+          backgroundColor: isHighlighted ? Colors.BLUE3 : null,
+          cursor: "pointer"
+        }}  // TODO: set ::before color to white
         onMouseEnter={() => this.onMouseEnter(idx)}
-        onMouseLeave={() => this.onMouseLeave(idx)}
+        onClick={() => this.onSelectEntry(entry.key)}
       >
         <MenuItem
+          className={s.bad}
           key={entry.key}
           text={(
             <div style={{ marginLeft: "25px" }}>
@@ -180,103 +282,28 @@ export default class PredictingPicker extends React.Component<PredictingPickerPr
           ) as any} // Cast required because text not declared as string | JSXElement (even though that works)
           label={(this.props.selectedKey === entry.key) ? IconContents.TICK : ""}
           iconName={this.props.entryIconName}
-          onClick={() => this.onSelectEntry(entry.key)}
         />
       </div>
     );
   }
-
-  // We cannot prevent the Blueprint :hover behaviour, thus if the mouse is currently hovered over an entry we have
-  // to disable the up/down arrow behaviour.  This is what mouseCaptured is for.
-  private onMouseEnter(idx: number) {
-    this.setState({ mouseCaptured: true, idxHighlighted: idx });
-  }
-
-  private onMouseLeave(_: number) {
-    this.setState({ mouseCaptured: false });
-  }
-
-  private onKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-    switch (e.which) {
-      case Keys.ARROW_DOWN:
-        if (!this.state.mouseCaptured) {
-          this.setState({ idxHighlighted: Math.min(_.size(this.filteredEntries()) - 1, this.state.idxHighlighted + 1) });
-        }
-        e.preventDefault();
-        break;
-      case Keys.ARROW_UP:
-        if (!this.state.mouseCaptured) {
-          this.setState({ idxHighlighted: Math.max(0, this.state.idxHighlighted - 1) });
-        }
-        e.preventDefault();
-        break;
-      case Keys.ENTER: {
-        const highlighted = _.chain(this.categorisedFilteredEntries())
-          .values()
-          .flatten()
-          .find(entry => entry.idx === this.state.idxHighlighted)
-          .value();
-        if (highlighted) {
-          this.onSelectEntry(highlighted.key);
-        }
-        e.preventDefault();
-        break;
-      }
-      default:
-        break;
-    }
-  }
-
-  private onSelectEntry(key: string) {
-    this.setState({ menuVisible: false });
-    this.props.onChange(key);
-  }
-
-  private onInteraction(nextOpenState: boolean) {
-    this.setState({
-      menuVisible: nextOpenState,
-      shouldFilter: false,
-      idxHighlighted: 0,
-      mouseCaptured: false,
-    });
-  }
-
-  private onChangeText(text: string) {
-    this.setState({
-      text,
-      menuVisible: true,
-      shouldFilter: true,
-      idxHighlighted: 0,
-      mouseCaptured: false,
-    });
-
-    const matchingEntry = _.find(this.props.entries, entry => this.stringInString(text, entry.name));
-    this.props.onChange(matchingEntry.key);
-  }
-
-  private categorisedFilteredEntries(): CategorisedEntries {
-    let idx = 0;
-    // TODO: make types work properly
-    const x: {} = _.chain(this.filteredEntries())
-      .groupBy(entry => entry.category)
-      .map((entries, category: string) => [category, _.map(entries, entry => ({idx: idx++, entry} as NumberedEntry))])
-      .object()
-      .value();
-    return x;
-  }
-
-  private filteredEntries(): PredictingPickerEntry[] {
-    // No filtering if disabled or if there's no text to filter by!
-    if (!this.state.shouldFilter || !this.state.text) {
-      return this.props.entries;
-    }
-
-    return _.chain(this.props.entries)
-      .filter(entry => this.stringInString(this.state.text, entry.name))
-      .value();
-  }
-
-  private stringInString(needle: string, haystack: string) {
-    return haystack.toLowerCase().includes(needle.toLowerCase());
-  }
 }
+
+function stringInString(needle: string, haystack: string) {
+  return haystack.toLowerCase().includes(needle.toLowerCase());
+}
+
+
+
+// TODO: move this logic to external
+// TODO: shouldFilter is false when first interacted with, switches to true as soon as typing 
+
+  // private filteredEntries(): PredictingPickerEntry[] {
+  //   // No filtering if disabled or if there's no text to filter by!
+  //   if (!this.state.shouldFilter || !this.state.text) {
+  //     return this.props.entries;
+  //   }
+
+  //   return _.chain(this.props.entries)
+  //     .filter(entry => this.stringInString(this.state.text, entry.name))
+  //     .value();
+  // }
