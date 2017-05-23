d213838565184e660857a611278e765f44add43c
Oliver Charlesworth <oliver@quartic.io>
2017-05-23 11:06:05 +0100

    Add noob defect-score InsightView

--- zeus-frontend/app/containers/InsightView/index.tsx
+++ zeus-frontend/app/containers/InsightView/index.tsx
@@ -1,132 +1,86 @@
 import * as React from "react";
-import { connect } from "react-redux";
 
-import { createStructuredSelector } from "reselect";
-import * as selectors from "../../redux/selectors";
 import * as classNames from "classnames";
-const s = require("./style.css");
+import * as _ from "underscore";
+import { appHistory } from "../../routes";
 
-import { Link } from "react-router";
+import {
+  Classes,
+} from "@blueprintjs/core";
+import Pane from "../../components/Pane";
 
-import { TimeChart } from "../../components";
-import { InsightSummary } from "../../components";
-// import { Classes } from "@blueprintjs/core";
-import { Insight,  Asset } from "../../models";
-
-// import { Map } from "../../components";
-
-interface IProps {
-  ui: any;
-  insights: [Insight];
-  assets: { [id: string]: Asset };
-  params: {
-    insightId: string;
-  };
+interface Entry {
+  rank: number;
+  name: string;
+  rsl: string;
+  description: string;
+  score: number;
 }
 
-interface IState {
-  datasetId: string;
-}
+const HIGHEST_DEFECT_ROADS: Entry[] = [
+  { rank: 1, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 2, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 3, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 4, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 5, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+];
 
-const joinAssets = (insight: Insight, assets: {[id: string]: Asset}) => {
-  return insight.assetIds.map(assetId => assets[assetId]);
-};
+const LOWEST_DEFECT_ROADS: Entry[] = [
+  { rank: 1, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 2, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 3, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 4, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+  { rank: 5, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
+];
 
-const Asset = ({ insight, asset }) => {
-  const failed = insight.unfailedAssetIds.indexOf(asset.id) === -1;
-    // HACK: I'm filtering out failures here because the data is all fake
-    return (
-    <div key={asset.id} className={classNames(s.subCard, "pt-card", "pt-elevation-2")}>
-            <h5>
-              <Link to={`/assets/${asset.id}`}>
-                {asset.clazz}-{asset.model.manufacturer}{asset.model.name}-{asset.serial}
-              </Link>
-            </h5>
-              { failed ? <span className="pt-tag pt-intent-danger" style={{float: "right"}}>Failed</span> :
-          <span className="pt-tag pt-intent-success" style={{ float: "right" }}>No failure</span> }
-            <TimeChart
-              yLabel="Voltage"
-              events={failed ? asset.events : asset.events.filter(ev => ev.type !== "failure")}
-              timeSeries={[]}
-            />
-          </div>);
-};
 
-class InsightView extends React.Component<IProps, IState> {
-  public state : IState = {
-    datasetId: null,
-  };
+class InsightView extends React.Component<{}, {}> {
 
   render() {
-    const insight = this.props.insights.filter(i => i.id === this.props.params.insightId)[0];
-    const assets = joinAssets(insight, this.props.assets);
     return (
-<div className={s.container}>
-  <div className={s.main}>
-    <div className={classNames(s.card, "pt-card", "pt-elevation-2")}>
-      <h2>{insight.title} <small>#{this.props.params.insightId}</small></h2>
-      <div className={s.subInsightContainer}>
-        { insight.subInsights ? insight.subInsights.map((sub, idx) =>
-        <div key={idx} className={classNames(s.subInsight, "pt-callout", "pt-intent-danger", sub.icon)}>
-          {sub.text}
-        </div>) : null }
+      <div>
+        {this.renderPane("Highest defects (2016)", HIGHEST_DEFECT_ROADS, { backgroundColor: "rgba(219, 55, 55, 0.4)" })}
+        {this.renderPane("Lowest defects (2016)", LOWEST_DEFECT_ROADS, { backgroundColor: "rgba(15, 153, 96, 0.4)" })}
       </div>
+    );
+  }
 
-      <InsightSummary insight={insight} assets={this.props.assets}/>
-        {/*<div className={s.plotControls}>
-        <label className="pt-label pt-inline" style={{marginBottom: 0}}>
-              Time Series
-              <div className={Classes.SELECT}>
-                  <select>
-                    <option key={0} value="temperature">Temperature</option>
-                    <option key={1} value="pressure">Pressure</option>
-                  </select>
-              </div>
-         </label>
-         </div>*/}
-        </div>
+  private renderPane(title: string, entries: Entry[], style: any) {
+    return (
+      <Pane title={title} iconName="error">
+        <table
+          className={classNames(Classes.TABLE, Classes.INTERACTIVE, Classes.TABLE_STRIPED, Classes.TABLE_CONDENSED)}
+          style={{ width: "100%", tableLayout: "fixed" }}
+        >
+          <thead>
+            <tr>
+              <th>Ranking</th>
+              <th>Road name</th>
+              <th>RSL</th>
+              <th>Section description</th>
+              <th>Defect score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {_.map(entries, entry => this.renderRow(style, entry))}
+          </tbody>
+        </table>
+      </Pane>
+    );
+  }
 
-     <div className={classNames(s.card, "pt-card", "pt-elevation-2")}>
-      {/*<Map
-        height={100}
-        locations={assets.map((asset) => asset.location)}
-        colors={assets.map(asset => insight.unfailedAssetIds.indexOf(asset.id) > -1 ? 1 : 0)}
-      />*/}
-      </div>
-
-        { assets.map(asset => <Asset key={asset.id} asset={asset} insight={insight} /> )}
-      </div>
-
-      <div className={s.right}>
-        <div className="pt-card pt-elevation-2">
-          <h2>Actions</h2>
-          <p>
-          4 / 10 of similar assets have not yet failed.
-          </p>
-          <p><b>Consider scheduling proactive maintenance.</b></p>
-          <Link
-            className="pt-button pt-intent-primary"
-            to={{ pathname: "/inventory", query: {clazz: "Signal"}}}
-          >
-            View
-          </Link>
-        </div>
-      </div>
-    </div>
+  private renderRow(style, entry: Entry) {
+    return (
+      <tr style={style} onClick={() => appHistory.push(`/assets/${encodeURIComponent(entry.rsl)}`)}>
+        <td>{entry.rank}</td>
+        <td>{entry.name}</td>
+        <td>{entry.rsl}</td>
+        <td>{entry.description}</td>
+        <td>{entry.score}</td>
+      </tr>
     );
   }
 }
 
-const mapDispatchToProps = {
-};
 
-const mapStateToProps = createStructuredSelector({
-  ui: selectors.selectUi,
-  insights: selectors.selectInsights,
-  assets: selectors.selectAssets,
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(InsightView);
+export default InsightView;
