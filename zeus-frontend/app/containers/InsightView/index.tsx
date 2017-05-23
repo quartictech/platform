32f94072dafd2856201ed79424a1181e55edc2f0
Alex Sparrow <alex@quartic.io>
2017-05-23 11:20:05 +0100

    Data

--- zeus-frontend/app/containers/InsightView/index.tsx
+++ zeus-frontend/app/containers/InsightView/index.tsx
@@ -18,21 +18,49 @@ interface Entry {
 }
 
 const HIGHEST_DEFECT_ROADS: Entry[] = [
-  { rank: 1, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 2, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 3, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 4, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 5, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-];
-
+  {
+    "rank": 1, "rsl": "D244/010_5E_01", "score": 11.044694762716798,
+    "description": "HAMPTON LN TO RIVERDALE RD (MILLBOURNE RD)", "name": "MILLBOURNE ROAD",
+  },
+  {
+    "rank": 2, "rsl": "D758/005_5E_01", "score": 10.984039245395659,
+    "description": "JERSEY RD TO PEVENSEY CL (S)", "name": "PEVENSEY CLOSE",
+  },
+  {
+    "rank": 3, "rsl": "D1181/050_5E_01", "score": 10.655653212181992,
+    "description": "STAVELEY GRDS TO END OF RD (STAVELEY GRDS EB)", "name": "STAVELEY GARDENS",
+  },
+  {
+    "rank": 4, "rsl": "D666/005_5E_01", "score": 10.323185274237224,
+    "description": "MONTAGUE RD TO END OF RD", "name": "YORK ROAD",
+  },
+  {
+    "rank": 5, "rsl": "D852/010_5E_01", "score": 10.270034203090121,
+    "description": "RBT TREVOR CL TO RBT TREVOR CL", "name": "TREVOR CLOSE",
+  },
+  ];
 const LOWEST_DEFECT_ROADS: Entry[] = [
-  { rank: 1, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 2, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 3, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 4, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-  { rank: 5, name: "Noob Road", rsl: "B454/030_3E_01", description: "Somewhere naughty", score: 1.96 },
-];
-
+  {
+    "rsl": "D2016/010_5E_01", "score": 0.0,
+    "description": "WORTON RD TO END OF RD (FLEMING WAY)", "name": "FLEMING WAY", "rank": 2533,
+  },
+  {
+    "rsl": "C013/525_3D_01", "score": 0.0,
+    "description": "ROSTREVOR GRDS TO THORNCLIFFE RD", "name": "NORTH HYDE LANE", "rank": 2532,
+  },
+  {
+    "rsl": "C014/515_3E_01", "score": 0.0,
+    "description": "JCT SPRINGWELL RD TO BERKELEY WAYE", "name": "CRANFORD LANE", "rank": 2531,
+  },
+  {
+    "rsl": "C014/515_3E_02", "score": 0.0,
+    "description": "FAIRMEAD CL TO END OF SLIP (CRANFORD LN)", "name": "CRANFORD LANE", "rank": 2530,
+  },
+  {
+    "rsl": "D757/005_5E_01", "score": 0.0,
+    "description": "JERSEY RD TO GREAT W RD (STUCLEY RD)", "name": "STUCLEY ROAD", "rank": 2529,
+  },
+  ];
 
 class InsightView extends React.Component<{}, {}> {
 
