f1ab6c2ee7e4147fd02e21fd40a66c2b8c31914b
Oliver Charlesworth <oliver@quartic.io>
2017-05-26 15:33:54 +0100

    WIP

--- zeus-frontend/app/containers/AssetView/index.tsx
+++ zeus-frontend/app/containers/AssetView/index.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 const DocumentTitle = require("react-document-title");  // TODO: wtf - doesn't work with import
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { TimeSeriesPoint, MaintenanceEvent } from "../../models";
 import Attributes from "./attributes";
+import DefectsChart from "./defectsChart";
 import Schematic from "./schematic";
-import EventsTable from "./events";
+import EventsTable from "./eventsTable";
 
 import {
   resourceActions,
@@ -16,7 +16,6 @@ import {
 import {
   Classes,
   NonIdealState,
-  Position,
   Spinner,
 } from "@blueprintjs/core";
 
@@ -29,9 +28,7 @@ import * as actions from "../../redux/actions";
 import { Asset } from "../../models";
 const s = require("./style.css");
 
-import { TimeChart } from "../../components/TimeChart";
 import { Map } from "../../components/Map";
-import NormalPicker from "../../components/NormalPicker";
 import Pane from "../../components/Pane";
 
 interface IProps {
@@ -44,16 +41,9 @@ interface IProps {
   assetRequired: (string) => void;
 }
 
-interface IState {
-  defectChartSelection: string;
-}
-
-class AssetView extends React.Component<IProps, IState> {
+class AssetView extends React.Component<IProps, {}> {
   constructor() {
     super();
-    this.state = {
-      defectChartSelection: null,
-    };
   }
 
   public componentWillMount() {
@@ -64,90 +54,11 @@ class AssetView extends React.Component<IProps, IState> {
     if (this.props.params.assetId !== nextProps.params.assetId) {
       this.onNewAsset(nextProps.params.assetId);
     }
-
-    if (!this.state.defectChartSelection &&
-      nextProps.asset.data &&
-      nextProps.asset.data._defect_time_series) {
-      const timeSeriesKeys = Object.keys(nextProps.asset.data._defect_time_series);
-      if (timeSeriesKeys.length > 0) {
-        this.setState({ defectChartSelection: timeSeriesKeys[0] });
-      }
-    }
   }
 
   private onNewAsset(assetId: string) {
     this.props.assetRequired(assetId);
     this.setState({ defectChartSelection: null });
-  }
-
-
-  private computeTimeSeries(asset): TimeSeriesPoint[] {
-    if (this.state.defectChartSelection) {
-      return asset._defect_time_series[this.state.defectChartSelection]
-        .series.map( ({ timestamp, value }) => ({x: new Date(timestamp), y: value }));
-    }
-    return [];
-  }
-
-  private computeEvents(asset): MaintenanceEvent[] {
-    const treatments = asset._treatments
-      .filter(job => job["Estimated Completion Date"] != null)
-      .map(job => ({
-        type: "maintenance",
-        detail: job["Treatment"],
-        timestamp: new Date(job["Estimated Completion Date"]),
-      }));
-
-    const jobs = asset._jobs.concat(asset._jobs_geo)
-      .filter(job => job["Start Date"] != null)
-      .filter(job =>
-          job["Type"] !== "Street Cleansing - Reactive Response" &&
-          job["Type"] !== "MNHL - Highway Lighting" &&
-          job["Type"] !== "Street Lighting - Reactive Fault Repairs")
-      .map(job => ({
-        type: "other",
-        detail: `${job["Type"]} (${job["Number"]})`,
-        timestamp: new Date(job["Start Date"]),
-      }));
-
-    return treatments.concat(jobs);
-  }
-
-  private renderDefectsChart(asset) {
-    const timeSeries = this.computeTimeSeries(asset);
-    const events = this.computeEvents(asset);
-    return (
-      <Pane
-        title="Defects vs. time"
-        iconName="error"
-        extraHeaderContent={asset._defect_time_series ? this.renderChartButtons(asset) : null}
-      >
-        { asset._defect_time_series ?
-          <TimeChart
-            yLabel={this.state.defectChartSelection}
-            events={events}
-            timeSeries={timeSeries}
-          /> :
-          <NonIdealState
-            visual="info"
-            title="No survey data available"
-          />
-         }
-      </Pane>
-    );
-  }
-
-  private renderChartButtons(asset) {
-    const charts = Object.keys(asset._defect_time_series);
-    return (
-      <NormalPicker
-        iconName="timeline-line-chart"
-        position={Position.TOP}
-        selected={this.state.defectChartSelection}
-        entries={charts}
-        onChange={id => this.setState({ defectChartSelection: id })}
-      />
-    );
   }
 
   private renderMap(asset) {
@@ -182,7 +93,7 @@ class AssetView extends React.Component<IProps, IState> {
                 {this.renderMap(asset.data)}
               </div>
             </div>
-            {this.renderDefectsChart(asset.data)}
+            <DefectsChart asset={asset.data} />
             <Schematic asset={asset.data} />
             <EventsTable asset={asset.data} />
           </div>
