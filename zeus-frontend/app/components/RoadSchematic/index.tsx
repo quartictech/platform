d2b54fdc13af792f9071288c2f923def0d9ffd34
Oliver Charlesworth <oliver@quartic.io>
2017-05-27 00:58:55 +0100

    Render variable-width XSPs

--- zeus-frontend/app/components/RoadSchematic/index.tsx
+++ zeus-frontend/app/components/RoadSchematic/index.tsx
@@ -8,6 +8,21 @@ import {
 import * as classNames from "classnames";
 import * as _ from "underscore";
 
+// TODO: error for 2015 for B3003/020_3E_01
+
+// See http://www.ukpms.com/owner_forum/shared_files/UKPMS_Manual_02_Chapter4_XSP_v04.pdf
+// TODO: what about off-road features?  And more lanes
+const ALL_LANES = ([
+  "LE",
+  "-L3", "-L2", "-L1",
+  "CL1", "CL2", "CL3",
+  "+L1", "+L2", "+L3",
+  "CC",
+  "+R3", "+R2", "+R1",
+  "CR3", "CR2", "CR1",
+  "-R1", "-R2", "-R3",
+  "RE",
+]);
 
 export interface RoadSchematicSection {
   xMin: number;
@@ -22,20 +37,36 @@ interface RoadSchematicProps {
   hoverText?: (RoadSchematicSection) => string | JSX.Element;
 }
 
-interface State {
+// Categorical scales don't allow heterogeneous width.  Thus we have to use a Linear scale, and manually
+// compute the y-axis interval corresponding to each XSP.
+interface XspToInterval {
+  [xsp: string] : [number, number];
+}
+
+interface PlotBits {
   dataset: Plottable.Dataset;
   colorScale: Plottable.Scales.InterpolatedColor;
   xScale: Plottable.Scales.Linear;
-  yScale: Plottable.Scales.Category;
-  plottableComponent: Plottable.Component;
-  hoveredSection: RoadSchematicSection;
+  yScale: Plottable.Scales.Linear;
+  outer: Plottable.Component;
 }
+
+interface State {
+  plot: PlotBits;
+  hoveredSection: RoadSchematicSection;
+  intervalMap: XspToInterval;
+}
+
 
 class RoadSchematic extends React.Component<RoadSchematicProps, State> {
   constructor(props: RoadSchematicProps) {
     super(props);
-    this.state = this.createInitialState();
-    this.setPlotData(props.sections); // Set initial data
+    this.state = {
+      plot: this.createInitialPlotState(),
+      hoveredSection: null,
+      intervalMap: intervalMap(props.sections),
+    };
+    this.setPlotData(props.sections); // Attach initial data to plot
   }
 
   // TODO - get the initial size to be regular (currently too big on the right)
@@ -57,74 +88,83 @@ class RoadSchematic extends React.Component<RoadSchematicProps, State> {
   }
 
   componentDidMount() {
-    window.addEventListener("resize", () => this.state.plottableComponent.redraw());
-    this.state.plottableComponent.renderTo(this.refs["svg"]);
+    window.addEventListener("resize", () => this.state.plot.outer.redraw());
+    this.state.plot.outer.renderTo(this.refs["svg"]);
   }
 
   componentWillUpdate(nextProps: RoadSchematicProps) {
     if (nextProps.sections !== this.props.sections || nextProps.maxValue !== this.props.maxValue) {
       this.setPlotData(nextProps.sections);
-      this.state.plottableComponent.redraw();
+      this.setState({ intervalMap: intervalMap(nextProps.sections) });
+      this.state.plot.outer.redraw();
     }
   }
 
   private setPlotData(sections: RoadSchematicSection[]) {
-    this.state.colorScale.domain([0, this.props.maxValue]);
-    this.state.xScale
+    this.state.plot.colorScale.domain([0, this.props.maxValue]);
+    this.state.plot.xScale
       .domainMin(_.min(sections, s => s.xMin).xMin || 0)
       .domainMax(_.max(sections, s => s.xMax).xMax || 0);
-    this.state.yScale.domain(this.createLaneDomain(sections));
-    this.state.dataset.data(sections);
+    this.state.plot.yScale
+      .domain([0, _.max(this.state.intervalMap, i => i[1])[1]])
+      .tickGenerator(() => _.map(this.state.intervalMap, i => (i[1] + i[0]) / 2));  // Ticks at interval midpoints
+    this.state.plot.dataset.data(sections);
   }
 
-  private createLaneDomain(sections: RoadSchematicSection[]) {
-    const currentLanes = _.chain(sections)
-      .map(s => s.lane)
-      .uniq()
-      .value();
-
-    // TODO: what about off-road features?  And more lanes
-    const allLanes = ([
-      "LE",
-      "-L3", "-L2", "-L1",
-      "CL1", "CL2", "CL3",
-      "+L1", "+L2", "+L3",
-      "CC",
-      "+R3", "+R2", "+R1",
-      "CR3", "CR2", "CR1",
-      "-R1", "-R2", "-R3",
-      "RE",
-    ]);
-
-    return _.filter(allLanes, l => _.contains(currentLanes, l));
-  }
-
-  private createInitialState(): State {
+  private createInitialPlotState(): PlotBits {
     const dataset = new Plottable.Dataset();
 
     const xScale = new Plottable.Scales.Linear();
-    const yScale = new Plottable.Scales.Category();
+    const yScale = new Plottable.Scales.Linear();
 
     const colorScale = new Plottable.Scales.InterpolatedColor()
       .range([Colors.GRAY1, Colors.RED4, Colors.RED3, Colors.RED2, Colors.RED1]);
 
-    const plot = new Plottable.Plots.Rectangle()
+    const newPlot = () => new Plottable.Plots.Rectangle()
       .addDataset(dataset)
       .x((s: RoadSchematicSection) => s.xMin, xScale)
       .x2((s: RoadSchematicSection) => s.xMax)
-      .y((s: RoadSchematicSection) => s.lane, yScale)
+      .y((s: RoadSchematicSection) => this.state.intervalMap[s.lane][0], yScale)
+      .y2((s: RoadSchematicSection) => this.state.intervalMap[s.lane][1]);
+
+    const plot = newPlot()
       .attr("fill", (s: RoadSchematicSection) => s.value, colorScale)
       .attr("stroke", Colors.LIGHT_GRAY5)
       .attr("stroke-width", 1);
 
-    const plotHighlighter = new Plottable.Plots.Rectangle()
-      .addDataset(dataset)
-      .x((s: RoadSchematicSection) => s.xMin, xScale)
-      .x2((s: RoadSchematicSection) => s.xMax)
-      .y((s: RoadSchematicSection) => s.lane, yScale)
+    const plotHighlighter = newPlot()
       .attr("fill", "black")
       .attr("fill-opacity", 0);
 
+    this.configureInteraction(plot, plotHighlighter);
+
+    const xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
+    const yAxis = new Plottable.Axes.Numeric(yScale, "left");
+
+    // Convert tick numeric positions to corresponding XSP names
+    yAxis
+      .tickLabelPadding(5)
+      .formatter(label => _.findKey(this.state.intervalMap, i => i[0] < label && label < i[1]).toString());
+
+    const yLabel = new Plottable.Components.AxisLabel("Lane", 270);
+
+    const group = new Plottable.Components.Group([plot, plotHighlighter]);
+
+    const outer = new Plottable.Components.Table([
+      [yLabel, yAxis, group],
+      [null, null,  xAxis],
+    ]);
+
+    return {
+      dataset,
+      colorScale,
+      xScale,
+      yScale,
+      outer,
+    };
+  }
+
+  private configureInteraction(plot: Plottable.Plot, plotHighlighter: Plottable.Plot) {
     const interaction = new Plottable.Interactions.Pointer();
     interaction.onPointerMove((p) => {
       const nearestEntity = plotHighlighter.entityNearest(p);
@@ -137,21 +177,33 @@ class RoadSchematic extends React.Component<RoadSchematicProps, State> {
       plotHighlighter.entities().forEach(entity => entity.selection.attr("fill-opacity", 0));
     });
     interaction.attachTo(plot);
+  }
+}
 
-    const xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
-    const yAxis = new Plottable.Axes.Category(yScale, "left");
+// The magic here is the reducer - it accumulates the y-axis position from interval to interval.
+const intervalMap = (sections: RoadSchematicSection[]) => _.chain(sections)
+  .map(s => s.lane)
+  .uniq()
+  .sortBy(lane => _.indexOf(ALL_LANES, lane))
+  .reduce(
+    (memo: [number, XspToInterval], lane) => {
+      const nextMax = memo[0] + laneWidth(lane);
+      const nextInterval = [memo[0], nextMax];
+      return [nextMax, { ...(memo[1]), [lane]: nextInterval}] as [number, XspToInterval]
+    },
+    [0, {}] as [number, XspToInterval]
+  )
+  .value()[1];
 
-    const yLabel = new Plottable.Components.AxisLabel("Lane", 270);
-
-    const group = new Plottable.Components.Group([plot, plotHighlighter]);
-
-    const plottableComponent = new Plottable.Components.Table([
-      [yLabel, yAxis, group],
-      [null, null,  xAxis],
-    ]);
-
-    return { dataset, colorScale, xScale, yScale, plottableComponent, hoveredSection: null };
-  } 
+const laneWidth = (lane: string) => {
+  switch (lane) {
+    case "LE":
+    case "RE":
+    case "CC":
+      return 0.5;
+    default:
+      return 1.0;
+  }
 }
 
 export default RoadSchematic;
