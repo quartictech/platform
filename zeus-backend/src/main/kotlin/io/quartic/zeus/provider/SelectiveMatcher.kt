acd1b4a28c2af79731c78825ff24170512ffd3b4
Oliver Charlesworth <oliver@quartic.io>
2017-05-30 17:09:11 +0100

    Precompute lower-cased versions of terms/values

--- zeus-backend/src/main/kotlin/io/quartic/zeus/provider/SelectiveMatcher.kt
+++ zeus-backend/src/main/kotlin/io/quartic/zeus/provider/SelectiveMatcher.kt
@@ -8,14 +8,15 @@ class SelectiveMatcher(
         private val data: Map<ItemId, Map<String, Any>>
 ) : TermMatcher {
     override fun invoke(terms: Set<String>, limit: Int): Map<ItemId, Map<String, Any>> {
+        val lowerCaseTerms = terms.map { it.toLowerCase() }
+
         // TODO: this needs optimising
         val filteredSequence = data
                 .asSequence()
                 .filter({ item ->
                     indexedAttributes.any {
-                        terms.any { term ->
-                            (item.value[it]?.toString() ?: "").toLowerCase().contains(term.toLowerCase())
-                        }
+                        val lowerCaseItem = (item.value[it]?.toString() ?: "").toLowerCase()
+                        lowerCaseTerms.any { term -> lowerCaseItem.contains(term) }
                     }
                 })
 
