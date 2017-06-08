40775185171ae356fe0602f7e55253646c014737
Oliver Charlesworth <oliver@quartic.io>
2017-06-08 17:31:01 +0100

    Add comment

--- zeus-frontend/app/containers/AssetView/attributes.tsx
+++ zeus-frontend/app/containers/AssetView/attributes.tsx
@@ -20,6 +20,7 @@ interface AttributesProps {
   asset: Asset;
 }
 
+// Add 40px to compensate for not having a title bar
 const Attributes: React.SFC<AttributesProps> = (props) => (
   <Pane style={{ backgroundColor: "rgba(138, 155, 168, 0.15)" }}>
     <div style={{ height: "340px", padding: "10px" }}>
