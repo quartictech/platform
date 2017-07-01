63d8391340b14510b94da9168b21814a9f6a0034
Oliver Charlesworth <oliver@quartic.io>
2017-07-01 14:10:48 +0100

    Fix a load of stuff for webpack 2

--- zeus-frontend/config/webpack/prod.js
+++ zeus-frontend/config/webpack/prod.js
@@ -1,30 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
-var baseConfig = require('./base');
+var baseConfig = require("./base");
 
 var config = Object.assign({}, baseConfig, {
   bail: true,
 
-  entry: [ 'app/index.tsx' ],
+  entry: [ "./src/app/index.tsx" ],
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production'),
-        'BUILD_VERSION': JSON.stringify(process.env.BUILD_VERSION),
+      "process.env":{
+        "NODE_ENV": JSON.stringify("production"),
+        "BUILD_VERSION": JSON.stringify(process.env.BUILD_VERSION),
       }
     }),
-    // TODO: This was failing due to sourceMap issues so I've disabled
+    // TODO: This was failing due to sourceMap issues so I"ve disabled
     // new webpack.optimize.UglifyJsPlugin({
     //   compressor: {
     //     warnings: false,
     //   }
     // }),
 		new CopyWebpackPlugin([{from: "src/public"}]),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin()
   ]
 });
 
