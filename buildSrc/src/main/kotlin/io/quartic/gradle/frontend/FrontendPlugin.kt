7fea87f5c6ec21be29b16ace862c8c5e89622d44
Oliver Charlesworth <oliver@quartic.io>
2017-07-15 20:33:37 +0100

    Migrate bundle/run tasks

--- buildSrc/src/main/kotlin/io/quartic/gradle/frontend/FrontendPlugin.kt
+++ buildSrc/src/main/kotlin/io/quartic/gradle/frontend/FrontendPlugin.kt
@@ -3,7 +3,10 @@ package io.quartic.gradle.frontend
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPlugin.JAR_TASK_NAME
 import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.plugins.ide.idea.IdeaPlugin
 import org.gradle.plugins.ide.idea.model.IdeaModel
 import java.io.File
@@ -17,17 +20,25 @@ class FrontendPlugin : Plugin<Project> {
     private class Applier(val project: Project) {
         private val ext = project.extensions.create(EXTENSION, FrontendExtension::class.java)
 
-        // TODO - switch to yarn/bin/yarn once Gradle build cache supports symlinks
         val yarnDir = File(project.buildDir, "yarn")
-        val yarnExecutable = File(yarnDir, "lib/node_modules/yarn/bin/yarn.js")
         val nodeModulesDir = project.file("node_modules")
+        val srcDir = project.file("src")
+        val configDir = project.file("config")
+
+        // TODO - switch to symlinks in .bin once Gradle build cache supports symlinks
+        val yarnExecutable = File(yarnDir, "lib/node_modules/yarn/bin/yarn.js")
+        val tsNodeExecutable = File(nodeModulesDir, "ts-node/dist/bin.js")
+        val webpackExecutable = File(nodeModulesDir, "webpack/bin/webpack.js")
 
         init {
-            val packageJsonTask = createPackageJsonGenerationTask()
-            val installYarnTask = createInstallYarnTask()
-            createInstallDependenciesTask(installYarnTask, packageJsonTask)
-            // TODO - bundle / run / lint tasks
+            val packageJson = createPackageJsonGenerationTask()
+            val installYarn = createInstallYarnTask()
+            val installDeps = createInstallDependenciesTask(installYarn, packageJson)
+            val bundle = createBundleTask(installDeps)
+            createRunTask(installDeps)
+            // TODO - lint tasks
             configureIdeaPlugin()
+            configureJavaPlugin(bundle)
         }
 
         private fun createPackageJsonGenerationTask() = task<PackageJsonGenerationTask>(
@@ -53,12 +64,12 @@ class FrontendPlugin : Plugin<Project> {
             commandLine = listOf("npm", "install", "--global", "--no-save", "--prefix", yarnDir, "yarn@$YARN_VERSION")
         }
 
-        private fun createInstallDependenciesTask(installYarnTask: Task, packageJsonTask: Task) = task<Exec>(
+        private fun createInstallDependenciesTask(installYarn: Task, packageJson: Task) = task<Exec>(
             INSTALL_DEPENDENCIES,
             "Installs node_modules dependencies."
         ) {
-            inputs.files(installYarnTask.outputs)
-            inputs.files(packageJsonTask.outputs)
+            inputs.files(installYarn.outputs)
+            inputs.files(packageJson.outputs)
             inputs.file(project.file("yarn.lock"))
 
             outputs.dir(nodeModulesDir)
@@ -73,10 +84,52 @@ class FrontendPlugin : Plugin<Project> {
             ) + if (System.getenv().containsKey("CI")) listOf("--frozen-lockfile") else emptyList()
         }
 
+        private fun createBundleTask(installDeps: Task) = task<Exec>(
+            BUNDLE,
+            "Creates asset bundle via Webpack."
+        ) {
+            configureCommonInputs(installDeps)
+
+            outputs.dir(File(project.buildDir, "webpack"))
+            outputs.cacheIf { true }                    // TODO - build-cache is going to be weird due to injecting project.version
+
+            environment = mapOf(
+                "NODE_ENV" to "production",             // TODO: this can probably be handled inside Webpack configuration
+                "BUILD_VERSION" to project.version
+            )
+
+            commandLine = listOf(webpackExecutable,
+                "--config", File(configDir, "webpack/prod.ts"),
+                "--profile", "--colors"
+            )
+        }
+
+        private fun createRunTask(installDeps: Task) = task<Exec>(
+            RUN,
+            "Runs development server."
+        ) {
+            configureCommonInputs(installDeps)
+
+            commandLine = listOf(tsNodeExecutable, File(srcDir, "server"))
+        }
+
+        private fun Exec.configureCommonInputs(installDeps: Task) {
+            inputs.files(installDeps.outputs)
+            inputs.file(project.file("tsconfig.json"))
+            inputs.dir(project.file("config"))
+            inputs.dir(project.file("src"))
+        }
+
         private fun configureIdeaPlugin() {
             project.plugins.apply(IdeaPlugin::class.java)
             val ext = project.extensions.getByType(IdeaModel::class.java)
             ext.module.excludeDirs.add(nodeModulesDir)
+        }
+
+        private fun configureJavaPlugin(bundle: Task) {
+            project.plugins.apply(JavaPlugin::class.java)
+
+            (project.tasks.getByName(JAR_TASK_NAME) as Jar).from(bundle)
         }
 
         private inline fun <reified T : Task> task(name: String, description: String, block: T.() -> Unit): T {
