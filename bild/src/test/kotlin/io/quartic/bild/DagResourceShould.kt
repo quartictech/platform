bdc8bc9aaed12ccc0d3fdeef406f40f5feec38c7
Alex Sparrow <alex@quartic.io>
2017-08-03 17:55:47 +0100

    Fix test

--- bild/src/test/kotlin/io/quartic/bild/DagResourceShould.kt
+++ bild/src/test/kotlin/io/quartic/bild/DagResourceShould.kt
@@ -1,7 +1,6 @@
 package io.quartic.bild
 
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
 import io.quartic.bild.model.CustomerId
 import io.quartic.bild.resource.DagResource
 import org.hamcrest.Matchers
@@ -12,22 +11,11 @@ import org.junit.Test
 class DagResourceShould {
     private val dag = mapOf("noob" to "yes")
     private val jobResults = mock<JobResultStore>()
-    private val resource = DagResource(jobResults)
-
-    init {
-        whenever(jobResults.getLatest(CustomerId("111")))
-            .thenReturn(JobResultStore.Record(null, dag))
-    }
+    private val resource = DagResource(jobResults, dag)
 
     @Test
     fun fetch_dag_for_customer() {
         val dag = resource.dag(CustomerId("111"))
         assertThat(dag, Matchers.equalTo(dag))
-    }
-
-    @Test
-    fun raise_404_if_non_existant() {
-        val dag = resource.dag(CustomerId("ladispute"))
-        assertThat(dag, Matchers.nullValue())
     }
 }
