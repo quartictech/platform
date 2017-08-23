6600d9b7c1c03d143a9c56e4ac5a38108d1697f7
Oliver Charlesworth <oliver@quartic.io>
2017-08-23 12:35:08 +0100

    Fix test

--- eval/src/test/kotlin/io/quartic/eval/utils/CoroutineUtilsShould.kt
+++ eval/src/test/kotlin/io/quartic/eval/utils/CoroutineUtilsShould.kt
@@ -2,6 +2,7 @@ package io.quartic.eval.utils
 
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.delay
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -9,9 +10,11 @@ import org.junit.Test
 class CoroutineUtilsShould {
     var b = false
 
+    // We're trying to test a suspended function call inside a finally block,
+    // as described in https://github.com/Kotlin/kotlinx.coroutines/blob/master/coroutines-guide.md#run-non-cancellable-block
     val sac = object : SuspendedAutoCloseable {
         suspend override fun close() {
-            delay(10)   // This causes an issue, corresponding to https://github.com/Kotlin/kotlinx.coroutines/blob/master/coroutines-guide.md#run-non-cancellable-block
+            delay(10)
             b = true
         }
     }
@@ -33,11 +36,15 @@ class CoroutineUtilsShould {
     fun invoke_close_properly_even_when_cancelled() {
         try {
             runOrTimeout {
+                val channel = Channel<Int>()
+
                 val job = async(CommonPool) {
                     sac.use {
+                        channel.send(1) // Release
                         delay(500)
                     }
                 }
+                channel.receive()       // Synchronise
                 job.cancel()
                 job.join()
             }
