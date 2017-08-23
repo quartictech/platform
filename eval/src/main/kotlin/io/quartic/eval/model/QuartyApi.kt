3760e11b83414fdf85d8c0ac087f2493deb17013
Alex Sparrow <alex@quartic.io>
2017-08-23 10:35:44 +0100

    Preliminary DAG stuff

---
+++ eval/src/main/kotlin/io/quartic/eval/model/QuartyApi.kt
@@ -0,0 +1,20 @@
+package io.quartic.eval.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Dataset(
+    val namespace: String?,
+    @JsonProperty("dataset_id")
+    val datasetId: String
+)
+
+data class Step(
+    val id: String,
+    val name: String,
+    val description: String,
+    val file: String,
+    @JsonProperty("line_range")
+    val lineRange: List<Int>,
+    val inputs: List<Dataset>,
+    val outputs: List<Dataset>
+)
