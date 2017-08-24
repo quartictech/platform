fde171ac57ac84117bbce97ad940d0fe776688fe
Alex Sparrow <alex@quartic.io>
2017-08-24 10:46:59 +0100

    Fixes for PR

--- eval/src/test/kotlin/io/quartic/eval/model/DagShould.kt
+++ eval/src/test/kotlin/io/quartic/eval/model/DagShould.kt
@@ -8,11 +8,24 @@ import org.hamcrest.Matchers.*
 import org.hamcrest.MatcherAssert.*
 
 class DagShould {
-    val steps =  OBJECT_MAPPER.readValue<List<Step>>(javaClass.getResourceAsStream("/steps.json"))
+    fun readSteps(file: String) =
+        OBJECT_MAPPER.readValue<List<Step>>(javaClass.getResourceAsStream(file))
 
     @Test
-    fun build_basaic_dag() {
-        val dag = Dag.fromSteps(steps)
+    fun build_basic_dag() {
+        val dag = Dag.fromSteps(readSteps("/steps.json"))
         assertThat(dag.validate(), equalTo(true))
+    }
+
+    @Test
+    fun detect_cyclic_dag() {
+        val dag = Dag.fromSteps(readSteps("/steps_cyclic.json"))
+        assertThat(dag.validate(), equalTo(false))
+    }
+
+    @Test
+    fun detect_multiple_steps_per_output() {
+        val dag = Dag.fromSteps(readSteps("/steps_duplicate_outputs.json"))
+        assertThat(dag.validate(), equalTo(false))
     }
 }
--- scripts/eval_trigger.sh
+++ scripts/eval_trigger.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+set -eu
 curl -H Content-Type:application/json -XPOST http://localhost:8210/api/trigger -d @test.json
