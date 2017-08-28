f391332f528e2d462725be17803c9d030610f8d6
Alex Sparrow <alex@quartic.io>
2017-08-28 10:07:35 +0100

    BuildResult

--- eval/src/main/kotlin/io/quartic/eval/apis/BuildResult.kt
+++ eval/src/main/kotlin/io/quartic/eval/apis/BuildResult.kt
@@ -6,10 +6,14 @@ import io.quartic.quarty.model.Step
 
 @JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.PROPERTY, property="type")
 sealed class BuildResult {
+    val version: Long = 1
+
     @JsonTypeName("success")
     data class Success(val dag: List<Step>) : BuildResult()
+
     @JsonTypeName("internal_error")
     data class InternalError(val throwable: Throwable) : BuildResult()
+
     @JsonTypeName("user_error")
     data class UserError(val detail: Any?) : BuildResult()
 }
--- quarty-utils/src/main/kotlin/io/quartic/quarty/model/Messages.kt
+++ quarty-utils/src/main/kotlin/io/quartic/quarty/model/Messages.kt
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes(
-    JsonSubTypes.Type(value = QuartyMessage.Progress::class, name = "progress"),
-    JsonSubTypes.Type(value = QuartyMessage.Log::class, name = "log"),
-    JsonSubTypes.Type(value = QuartyMessage.Result::class, name = "result"),
-    JsonSubTypes.Type(value = QuartyMessage.Error::class, name = "error")
-)
 sealed class QuartyMessage {
+    @JsonSubTypes.Type(value = QuartyMessage.Progress::class, name = "progress")
     data class Progress(val message: String) : QuartyMessage()
+
+    @JsonSubTypes.Type(value = QuartyMessage.Log::class, name = "log")
     data class Log(val stream: String, val line: String) : QuartyMessage()
+
+    @JsonSubTypes.Type(value = QuartyMessage.Result::class, name = "result")
     data class Result(val result: List<Step>) : QuartyMessage()
+
+    @JsonSubTypes.Type(value = QuartyMessage.Error::class, name = "error")
     data class Error(val detail: Any) : QuartyMessage()
 }
