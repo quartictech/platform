79f36e41fee0e50b6128568610900283c636b7e5
Oliver Charlesworth <oliver@quartic.io>
2017-09-02 10:36:51 +0100

    Delete ES-specific frontend stuff

--- buildSrc/src/main/kotlin/io/quartic/gradle/frontend/CommonDependencies.kt
+++ buildSrc/src/main/kotlin/io/quartic/gradle/frontend/CommonDependencies.kt
@@ -21,36 +21,7 @@ val standardDependencies = mapOf(
     "webpack" to "2.6.1",
     "webpack-merge" to "4.1.0",
 
-    // Weyl-specific (TODO: consolidate Webpack config and merge this with the above)
-    "babel-eslint" to "6.1.2",
-    "babel-plugin-react-transform" to "2.0.2",
-    "babel-plugin-transform-react-constant-elements" to "6.9.1",
-    "babel-plugin-transform-react-inline-elements" to "6.8.0",
-    "babel-plugin-transform-react-remove-prop-types" to "0.2.9",
-    "babel-preset-react" to "6.11.1",
-    "babel-preset-react-hmre" to "1.1.1",
-    "babel-preset-stage-0" to "6.5.0",
-    "eventsource-polyfill" to "0.9.6",
-    "exports-loader" to "0.6.3",
-    "extract-text-webpack-plugin" to "2.1.2",
-    "html-loader" to "0.4.3",
-    "html-webpack-plugin" to "2.22.0",
-    "image-webpack-loader" to "2.0.0",
-    "imports-loader" to "0.6.5",
-    "postcss-focus" to "1.0.0",
-    "postcss-reporter" to "1.4.1",
-
     // Lint
-    "eslint" to "3.5.0",
-    "eslint-config-airbnb" to "10.0.0",
-    "eslint-import-resolver-webpack" to "0.4.0",
-    "eslint-plugin-import" to "1.12.0",
-    "eslint-plugin-jsx-a11y" to "2.0.1",
-    "eslint-plugin-react" to "6.0.0",
-
-    "stylelint" to "7.1.0",
-    "stylelint-config-standard" to "12.0.0",
-
     "tslint" to "5.4.3",
     "tslint-config-airbnb" to "5.2.1",
     "tslint-react" to "3.0.0",
