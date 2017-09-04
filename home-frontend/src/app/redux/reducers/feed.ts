569c538ff613259d20ec440f6d2cb40313b46715
Alex Sparrow <alex@quartic.io>
2017-09-04 19:08:23 +0100

    Visual tweaks

--- home-frontend/src/app/redux/reducers/feed.ts
+++ home-frontend/src/app/redux/reducers/feed.ts
@@ -1,13 +1,24 @@
 import { DatasetAction } from "../../models";
 import * as constants from "../constants";
-import { List, fromJS } from "immutable";
+import { fromJS } from "immutable";
 
-const initialState = List<any>();
+const initialState = fromJS({
+  state: "not_loaded",
+  items: [],
+});
 
-export function feedReducer(state: List<any> = initialState, action: DatasetAction) {
+export function feedReducer(state = initialState, action: DatasetAction) {
   switch (action.type) {
     case constants.FETCH_FEED_SUCCESS:
-      return fromJS(action.data);
+      return fromJS({
+        state: "loaded",
+        items: action.data,
+      });
+    case constants.FETCH_FEED:
+      return fromJS({
+        state: "loading",
+        items: [],
+      });
     default:
       return state;
   }
