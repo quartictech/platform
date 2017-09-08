777fb12ca2a2432789084d8f62fa814ad48f7480
Oliver Charlesworth <oliver@quartic.io>
2017-09-08 09:36:09 +0100

    WIP

--- common-db/src/main/kotlin/io/quartic/common/db/DatabaseBuilder.kt
+++ common-db/src/main/kotlin/io/quartic/common/db/DatabaseBuilder.kt
@@ -2,6 +2,7 @@ package io.quartic.common.db
 
 import com.github.arteam.jdbi3.JdbiFactory
 import com.github.arteam.jdbi3.strategies.TimedAnnotationNameStrategy
+import io.dropwizard.db.DataSourceFactory
 import io.dropwizard.setup.Environment
 import io.quartic.common.ApplicationDetails
 import io.quartic.common.logging.logger
@@ -13,32 +14,38 @@ import org.jdbi.v3.core.Jdbi
 import javax.sql.DataSource
 
 class DatabaseBuilder(
-    val owner: Class<*>,
-    val configuration: DatabaseConfiguration,
-    val environment: Environment,
-    val secretsCodec: SecretsCodec) {
+    private val owner: Class<*>,
+    private val datasourceFactory: DataSourceFactory,
+    private val environment: Environment,
+    private val migrationVersion: MigrationVersion = MigrationVersion.LATEST
+) {
+    constructor(
+        owner: Class<*>,
+        configuration: DatabaseConfiguration,
+        environment: Environment,
+        secretsCodec: SecretsCodec,
+        migrationVersion: MigrationVersion = MigrationVersion.LATEST
+    ) : this(
+        owner,
+        datasourceFactory(owner, configuration, secretsCodec),
+        environment,
+        migrationVersion
+    )
 
+    val dbi by lazy {
+        migrate(owner, datasourceFactory.build(environment.metrics(), "flyway"), migrationVersion)
 
-    inline fun <reified T> dao(): T {
-        val factory = configuration.dataSourceFactory(secretsCodec)
+        val dbi = JdbiFactory(TimedAnnotationNameStrategy()).build(environment, datasourceFactory, "postgres")
 
-        val details = ApplicationDetails(owner)
-        factory.validationQuery = "/* ${details.name} - ${details.version} Health Check */ SELECT 1"
-        factory.properties = mutableMapOf(
-            "ApplicationName" to "${details.name} - ${details.version}"
-        )
-
-        migrate(owner, factory.build(environment.metrics(), "flyway"), MigrationVersion.LATEST)
-
-        val dbi = JdbiFactory(TimedAnnotationNameStrategy()).build(environment, factory, "postgres")
-        return setupDbi(dbi).onDemand(T::class.java)
+        setupDbi(dbi)
     }
+
+    inline fun <reified T> dao(): T = dbi.onDemand(T::class.java)
 
     companion object {
         val LOG by logger()
 
-        fun migrate(owner: Class<*>, dataSource: DataSource,
-                    migrationVersion: MigrationVersion) {
+        fun migrate(owner: Class<*>, dataSource: DataSource, migrationVersion: MigrationVersion) {
             val flyway = Flyway()
             flyway.dataSource = dataSource
             flyway.classLoader = owner.classLoader
@@ -51,11 +58,26 @@ class DatabaseBuilder(
             }
         }
 
-        inline fun <reified T> testDao(owner: Class<*>, dataSource: DataSource,
-                                       migrationVersion: MigrationVersion = MigrationVersion.LATEST): T {
+        inline fun <reified T> testDao(
+            owner: Class<*>,
+            dataSource: DataSource,
+            migrationVersion: MigrationVersion = MigrationVersion.LATEST
+        ): T {
             migrate(owner, dataSource, migrationVersion)
             val dbi = Jdbi.create(dataSource)
             return setupDbi(dbi).onDemand(T::class.java)
+        }
+
+        private fun datasourceFactory(
+            owner: Class<*>,
+            configuration: DatabaseConfiguration,
+            secretsCodec: SecretsCodec
+        ): DataSourceFactory {
+            val details = ApplicationDetails(owner)
+            val factory = configuration.dataSourceFactory(secretsCodec)
+            factory.validationQuery = "/* ${details.name} - ${details.version} Health Check */ SELECT 1"
+            factory.properties = mutableMapOf("ApplicationName" to "${details.name} - ${details.version}")
+            return factory
         }
     }
 }
