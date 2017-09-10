96b15a3c0fbf54f27760b3af4e7201e1995cd33d
Alex Sparrow <alex@quartic.io>
2017-09-10 16:52:15 +0100

    Bump stuff

--- buildSrc/src/main/kotlin/io/quartic/gradle/frontend/CommonDependencies.kt
+++ buildSrc/src/main/kotlin/io/quartic/gradle/frontend/CommonDependencies.kt
@@ -2,23 +2,23 @@ package io.quartic.gradle.frontend
 
 val standardDependencies = mapOf(
     // Webpack build
-    "babel-core" to "6.18.2",
-    "babel-loader" to "6.4.1",
+    "babel-core" to "6.26.0",
+    "babel-loader" to "7.1.2",
     "babel-plugin-transform-runtime" to "^6.23.0",
     "babel-preset-es2015" to "^6.24.1",
     "copy-webpack-plugin" to "4.0.1",
-    "css-loader" to "0.23.1",
-    "file-loader" to "0.8.5",
-    "json-loader" to "0.5.4",
-    "postcss-cssnext" to "2.9.0",
+    "css-loader" to "0.28.7",
+    "file-loader" to "0.11.2",
+    "json-loader" to "0.5.7",
+    "postcss-cssnext" to "3.0.2",
     "postcss-loader" to "2.0.6",
     "react-hot-loader" to "1.3.1",
-    "source-map-loader" to "0.1.5",
-    "style-loader" to "0.13.1",
+    "source-map-loader" to "0.2.1",
+    "style-loader" to "0.18.2",
     "ts-loader" to "2.3.4",
     "typescript" to "2.5.0",
     "url-loader" to "0.5.7",
-    "webpack" to "2.6.1",
+    "webpack" to "3.5.6",
     "webpack-merge" to "4.1.0",
 
     // Lint
