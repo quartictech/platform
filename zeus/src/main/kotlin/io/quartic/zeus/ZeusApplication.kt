ddf80dbd7d5958a86291d9041753bcd8c3196ac3
Oliver Charlesworth <oliver@quartic.io>
2017-09-10 11:22:04 +0100

    Decouple frontend subprojects from application subprojects

--- zeus/src/main/kotlin/io/quartic/zeus/ZeusApplication.kt
+++ zeus/src/main/kotlin/io/quartic/zeus/ZeusApplication.kt
@@ -1,7 +1,5 @@
 package io.quartic.zeus
 
-import io.dropwizard.assets.AssetsBundle
-import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import io.quartic.common.application.ApplicationBase
 import io.quartic.common.logging.logger
@@ -12,10 +10,6 @@ import io.quartic.zeus.resource.SessionInfoResource
 
 class ZeusApplication : ApplicationBase<ZeusConfiguration>() {
     private val LOG by logger()
-
-    public override fun initializeApplication(bootstrap: Bootstrap<ZeusConfiguration>) {
-        bootstrap.addBundle(AssetsBundle("/assets", "/", "index.html"))
-    }
 
     override fun runApplication(configuration: ZeusConfiguration, environment: Environment) {
         with (environment.jersey()) {
