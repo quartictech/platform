770533b9e370e5567333702a9e7ae0b7b01dddda
Arlo Bryer <arlo@quartic.io>
2017-09-12 15:59:14 +0100

    add Build model back in.

---
+++ home-frontend/src/app/models/Models.ts
@@ -0,0 +1,104 @@
+export interface IDatasetCoords {
+  namespace: string;
+  id: string;
+}
+
+export interface IDatasetMetadata {
+  name: string;
+  description: string;
+  attribution: string;
+  icon?: string;
+}
+
+export interface IDatasetLocator {
+  type: string;
+}
+
+export interface IFiles {
+  files: any[];
+  fileType: string;
+}
+
+export interface IDataset {
+  metadata: IDatasetMetadata;
+  locator: IDatasetLocator;
+}
+
+export type DatasetMap = { [namespace: string]: { [id: string]: IDataset } };
+
+export interface ICreateDataset {
+  metadata: IDatasetMetadata;
+  files: IFiles;
+}
+
+export interface DatasetAction {
+  type: string;
+  data: any;
+}
+
+export interface PipelineAction {
+  type: string;
+  data: any;
+}
+
+export interface ProfileAction {
+  type: string;
+  data: any;
+}
+
+export interface UiAction {
+  type: string;
+  activeModal?: string;
+  search?: string;
+  namespace?: string;
+}
+
+export interface Ui {
+  namespace: string;
+  searchString: string;
+  activeModal: string;
+}
+
+export interface Profile {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface FeedItem {
+  type: string;
+  id: string;
+  time: number;
+}
+
+export interface Build extends FeedItem {
+  buildNumber: number;
+  branch: string;
+  status: string;
+  triggerDetails: {
+    type: string;
+  };
+}
+
+export interface Validate extends FeedItem {
+  buildNumber: number;
+  branch: string;
+  status: string;
+  triggerDetails: {
+    type: string;
+  };
+}
+
+export interface Execute extends FeedItem {
+  buildNumber: number;
+  branch: string;
+  status: string;
+  triggerDetails: {
+    type: string;
+  };
+}
+
+export enum LoadingState {
+  NOT_LOADED,
+  LOADING,
+  LOADED,
+}
