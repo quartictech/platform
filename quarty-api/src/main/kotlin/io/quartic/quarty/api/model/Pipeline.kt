3fd39ca64436eda9b512b8c769216b0f5e778cc4
Oliver Charlesworth <oliver@quartic.io>
2017-09-13 09:33:05 +0100

    Add new pipeline model

--- quarty-api/src/main/kotlin/io/quartic/quarty/api/model/Pipeline.kt
+++ quarty-api/src/main/kotlin/io/quartic/quarty/api/model/Pipeline.kt
@@ -2,6 +2,54 @@ package io.quartic.quarty.api.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
+import io.quartic.quarty.api.model.Node.Raw
+import io.quartic.quarty.api.model.Node.Step
+import io.quartic.quarty.api.model.Source.S3
+
+data class Pipeline(
+    val steps: List<Node>
+)
+
+@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type")
+@JsonSubTypes(
+    Type(Step::class, name = "step"),
+    Type(Raw::class, name = "raw")
+)
+sealed class Node {
+    abstract val id: String
+    abstract val info: LexicalInfo
+    abstract val inputs: List<Dataset>
+    abstract val outputs: List<Dataset>
+
+    data class Step(
+        override val id: String,
+        override val info: LexicalInfo,
+        override val inputs: List<Dataset>,
+        override val outputs: List<Dataset>
+    ) : Node()
+
+    data class Raw(
+        override val id: String,
+        override val info: LexicalInfo,
+        override val outputs: List<Dataset>,
+        val source: Source
+    ) : Node() {
+        override val inputs = emptyList<Dataset>()
+    }
+}
+
+data class LexicalInfo(
+    val name: String,
+    val description: String?,
+    val file: String,
+    @JsonProperty("line_range")
+    val lineRange: List<Int>
+)
 
 data class Dataset(
     val namespace: String?,
@@ -12,17 +60,13 @@ data class Dataset(
     val fullyQualifiedName get() = "${namespace ?: ""}::${datasetId}"
 }
 
-data class Step(
-    val id: String,
-    val name: String,
-    val description: String?,
-    val file: String,
-    @JsonProperty("line_range")
-    val lineRange: List<Int>,
-    val inputs: List<Dataset>,
-    val outputs: List<Dataset>
+@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type")
+@JsonSubTypes(
+    Type(S3::class, name = "s3")
 )
-
-data class Pipeline(
-    val steps: List<Step>
-)
+sealed class Source {
+    data class S3(
+        val bucket: String,
+        val key: String
+    ) : Source()
+}
