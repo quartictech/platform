f90d00a4ad0d18ed3dacb42bc38311af8159e405
Oliver Charlesworth <oliver@quartic.io>
2017-09-18 10:19:27 +0100

    Howl raw endpoint (#402)
    * Move stuff under /managed
    
    * Standardise names
    
    * Start building a StorageCoords hierarchy
    
    * Wire through Unmanaged
    
    * Eliminate 2D variants
    
    * Add 2D->3D migration
    
    * Fix migration
    
    * Fix path

--- howl-api/src/main/kotlin/io/quartic/howl/api/HowlClient.kt
+++ howl-api/src/main/kotlin/io/quartic/howl/api/HowlClient.kt
@@ -6,13 +6,14 @@ import okio.BufferedSink
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.URI
+import javax.ws.rs.core.HttpHeaders.USER_AGENT
 
 class HowlClient(userAgent: String, private val baseUrl: URI) : HowlService {
     private class UserAgentInterceptor(private val userAgent: String) : Interceptor {
         override fun intercept(chain: Interceptor.Chain): Response {
             val originalRequest = chain.request()
             val requestWithUserAgent = originalRequest.newBuilder()
-                    .header("User-Agent", userAgent)
+                    .header(USER_AGENT, userAgent)
                     .build()
             return chain.proceed(requestWithUserAgent)
         }
@@ -23,19 +24,12 @@ class HowlClient(userAgent: String, private val baseUrl: URI) : HowlService {
             .build()
 
     override fun uploadFile(
-            targetNamespace: String,
-            fileName: String,
-            contentType: String,
-            upload: (OutputStream) -> Unit
-    ) = uploadFile(url(targetNamespace, fileName), contentType, upload)
-
-    override fun uploadFile(
-            targetNamespace: String,
-            identityNamespace: String,
-            fileName: String,
-            contentType: String,
-            upload: (OutputStream) -> Unit
-    ) = uploadFile(url(targetNamespace, identityNamespace, fileName), contentType, upload)
+        targetNamespace: String,
+        identityNamespace: String,
+        key: String,
+        contentType: String,
+        upload: (OutputStream) -> Unit
+    ) = uploadFile(url(targetNamespace, MANAGED, identityNamespace, key), contentType, upload)
 
     private fun uploadFile(url: HttpUrl, contentType: String, upload: (OutputStream) -> Unit) {
         val request = Request.Builder()
@@ -47,16 +41,10 @@ class HowlClient(userAgent: String, private val baseUrl: URI) : HowlService {
 
     override fun uploadAnonymousFile(
             targetNamespace: String,
-            contentType: String,
-            upload: (OutputStream) -> Unit
-    ) = uploadAnonymousFile(url(targetNamespace), contentType, upload)
-
-    override fun uploadAnonymousFile(
-            targetNamespace: String,
             identityNamespace: String,
             contentType: String,
             upload: (OutputStream) -> Unit
-    ) = uploadAnonymousFile(url(targetNamespace, identityNamespace), contentType, upload)
+    ) = uploadAnonymousFile(url(targetNamespace, MANAGED, identityNamespace), contentType, upload)
 
     private fun uploadAnonymousFile(url: HttpUrl, contentType: String, upload: (OutputStream) -> Unit): HowlStorageId {
         val request = Request.Builder()
@@ -66,11 +54,11 @@ class HowlClient(userAgent: String, private val baseUrl: URI) : HowlService {
         return decode(client.newCall(request).execute().body()!!.string(), HowlStorageId::class.java)
     }
 
-    override fun downloadFile(targetNamespace: String, fileName: String)
-            = downloadFile(url(targetNamespace, fileName))
+    override fun downloadUnmanagedFile(targetNamespace: String, key: String)
+        = downloadFile(url(targetNamespace, UNMANAGED, key))
 
-    override fun downloadFile(targetNamespace: String, identityNamespace: String, fileName: String)
-            = downloadFile(url(targetNamespace, identityNamespace, fileName))
+    override fun downloadManagedFile(targetNamespace: String, identityNamespace: String, key: String)
+        = downloadFile(url(targetNamespace, MANAGED, identityNamespace, key))
 
     private fun downloadFile(url: HttpUrl): InputStream? {
         val request = Request.Builder()
@@ -102,5 +90,10 @@ class HowlClient(userAgent: String, private val baseUrl: URI) : HowlService {
                 upload(sink.outputStream())
             }
         }
+    }
+
+    companion object {
+        private val UNMANAGED = "unmanaged"
+        private val MANAGED = "managed"
     }
 }
--- howl/src/main/kotlin/io/quartic/howl/storage/S3KeyMapper.kt
+++
@@ -1,8 +0,0 @@
-package io.quartic.howl.storage
-
-fun mapForS3(coords: StorageCoords) =
-    if (coords.objectName.startsWith("raw/")) {
-        coords.objectName
-    } else {
-        ".quartic/${coords.identityNamespace}/${coords.objectName}"
-    }
