ab69c88f4999189f76f5fbdbeddb5ebe42f1f55b
Oliver Charlesworth <oliver@quartic.io>
2017-09-18 17:36:50 +0100

    Feature/get rid of home file conversion (#404)
    * Move stuff under /managed
    
    * Standardise names
    
    * Start building a StorageCoords hierarchy
    
    * Wire through Unmanaged
    
    * Eliminate 2D variants
    
    * Add 2D->3D migration
    
    * Fix migration
    
    * Get rid of Home file conversion
    
    * Delete obsolete converter things
    
    * Display stack-traces in yellow in logs
    
    * Mostly fix file upload

--- home/src/main/kotlin/io/quartic/home/CreateDatasetRequest.kt
+++ home/src/main/kotlin/io/quartic/home/CreateDatasetRequest.kt
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
-        Type(value = CreateStaticDatasetRequest::class, name = "static")
+    Type(value = CreateStaticDatasetRequest::class, name = "static")
 )
 interface CreateDatasetRequest
