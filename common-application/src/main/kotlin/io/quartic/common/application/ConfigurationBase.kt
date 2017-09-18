8163329dc818eacf092bf9dd713058cffcdf3c39
Oliver Charlesworth <oliver@quartic.io>
2017-09-18 10:28:16 +0100

    Eliminate config template

--- common-application/src/main/kotlin/io/quartic/common/application/ConfigurationBase.kt
+++ common-application/src/main/kotlin/io/quartic/common/application/ConfigurationBase.kt
@@ -1,35 +1,65 @@
 package io.quartic.common.application
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME
 import io.dropwizard.Configuration
 import io.dropwizard.jetty.HttpConnectorFactory
+import io.dropwizard.logging.ConsoleAppenderFactory
+import io.dropwizard.logging.DefaultLoggingFactory
 import io.dropwizard.server.DefaultServerFactory
 import io.quartic.common.secrets.EncryptedSecret
 import io.quartic.common.secrets.SecretsCodec
 import io.quartic.common.secrets.UnsafeSecret
 
 abstract class ConfigurationBase : Configuration() {
-    val masterKeyBase64: UnsafeSecret = DEV_MASTER_KEY_BASE64
+    val masterKeyBase64: UnsafeSecret = with(System.getenv("MASTER_KEY_BASE64")) {
+        if (this != null) {
+            UnsafeSecret(this)
+        } else {
+            DEV_MASTER_KEY_BASE64
+        }
+    }
+    
     val auth: AuthConfiguration = DummyAuthConfiguration()  // TODO - remove this default eventually
 
     // Opinionated port selection
     var url: ServerDetails = ServerDetails()
-        set(value) {
-            super.setServerFactory(DefaultServerFactory().apply {
-                applicationContextPath = value.contextPath
-                applicationConnectors = listOf(HttpConnectorFactory().apply {
-                    port = if (value.randomPort) 0 else value.port
-                })
-                adminConnectors = listOf(HttpConnectorFactory().apply {
-                    port = if (value.randomPort) 0 else (value.port + 1)
-                })
-            })
-        }
+        set(value) = configureServer(value)
 
-    val secretsCodec by lazy { SecretsCodec(masterKeyBase64) }
+    val secretsCodec by lazy {
+        SecretsCodec(masterKeyBase64)
+    }
+
+    init {
+        configureLogging()
+    }
+
+    // Opinionated server configuration
+    private fun configureServer(value: ServerDetails) {
+        super.setServerFactory(DefaultServerFactory().apply {
+            applicationContextPath = value.contextPath
+            applicationConnectors = listOf(HttpConnectorFactory().apply {
+                port = if (value.randomPort) 0 else value.port
+            })
+            adminConnectors = listOf(HttpConnectorFactory().apply {
+                port = if (value.randomPort) 0 else (value.port + 1)
+            })
+        })
+    }
+
+    // Opinionated logging configuration
+    private fun configureLogging() {
+        super.setLoggingFactory(DefaultLoggingFactory().apply {
+            level = Level.INFO
+            setAppenders(listOf(ConsoleAppenderFactory<ILoggingEvent>().apply {
+                logFormat = "%d{ISO8601, UTC} %highlight(%-5level) [%logger{36}]: %msg%n%rEx"
+            }))
+        })
+    }
 }
 
 data class ServerDetails(
