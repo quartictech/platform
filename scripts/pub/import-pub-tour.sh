bb770d22a298c1ee8e22a12606306e765c51006e
Oliver Charlesworth <oliver@quartic.io>
2017-09-20 21:28:58 +0100

    Fix scripts to use snake-case

--- scripts/pub/import-pub-tour.sh
+++ scripts/pub/import-pub-tour.sh
@@ -7,16 +7,6 @@ curl -H Content-Type:application/json -XPUT http://localhost:8090/api/datasets/n
       "description": "Arlo and Alex go wild",
       "attribution": "Quartic"
     },
-    "extensions": {
-        "map": {
-            "viewType": "LOCATION_AND_TRACK",
-            "titleAttribute": "name",
-            "blessedAttributes": [ "name" ],
-            "attributeTypes": {
-              "timestamp": "TIMESTAMP"
-            }
-        }
-    },
     "locator": {
         "type": "websocket",
         "url": "ws://localhost:5000/"
