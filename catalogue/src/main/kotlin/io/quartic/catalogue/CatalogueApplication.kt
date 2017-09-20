b5225de66b11d49245eb7cfe3fe114b311d2d870
Oliver Charlesworth <oliver@quartic.io>
2017-09-20 20:19:26 +0100

    Nuke all Datastore stuff

--- catalogue/src/main/kotlin/io/quartic/catalogue/CatalogueApplication.kt
+++ catalogue/src/main/kotlin/io/quartic/catalogue/CatalogueApplication.kt
@@ -4,7 +4,6 @@ import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 import io.dropwizard.websockets.WebsocketBundle
 import io.quartic.catalogue.database.Database
-import io.quartic.catalogue.migration.BackendToDatabaseMigration
 import io.quartic.common.application.ApplicationBase
 import io.quartic.common.db.DatabaseBuilder
 import io.quartic.common.websocket.serverEndpointConfig
@@ -18,11 +17,7 @@ class CatalogueApplication : ApplicationBase<CatalogueConfiguration>() {
     }
 
     public override fun runApplication(configuration: CatalogueConfiguration, environment: Environment) {
-        val backend = configuration.backend.build()
         val database = database(configuration, environment)
-
-        BackendToDatabaseMigration().migrate(backend, database)     // TODO - remove this once deployed
-
         val catalogue = CatalogueResource(database)
         environment.jersey().register(catalogue)
         websocketBundle.addEndpoint(serverEndpointConfig("/api/datasets/watch", catalogue))
