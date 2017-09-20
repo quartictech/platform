3f0123668b645313908b2e0c42d32c0a4ee5e242
Oliver Charlesworth <oliver@quartic.io>
2017-09-20 18:21:43 +0100

    Get backend working

--- catalogue/src/main/kotlin/io/quartic/catalogue/postgres/Database.kt
+++ catalogue/src/main/kotlin/io/quartic/catalogue/postgres/Database.kt
@@ -15,7 +15,6 @@ import org.jdbi.v3.sqlobject.customizer.Bind
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 import org.jdbi.v3.sqlobject.statement.SqlUpdate
 import java.sql.ResultSet
-import java.time.Instant
 
 @RegisterRowMapper(DatasetRowMapper::class)
 @RegisterColumnMapper(ConfigColumnMapper::class)
@@ -27,23 +26,37 @@ interface Database {
         val config: DatasetConfig?
     )
 
-    class DatasetRowMapper : RowMapper<DatasetConfig?> {
-        override fun map(rs: ResultSet, ctx: StatementContext) = rs.getString("config").let { parseConfigOrNull(it) }
+    class DatasetRowMapper : RowMapper<DatasetConfig> {
+        override fun map(rs: ResultSet, ctx: StatementContext) =
+            OBJECT_MAPPER.readValue<DatasetConfig>(rs.getString("config"))
     }
 
-    class ConfigColumnMapper : ColumnMapper<DatasetConfig?> {
-        override fun map(r: ResultSet, columnNumber: Int, ctx: StatementContext) = r.getString(columnNumber).let { parseConfigOrNull(it) }
+    class ConfigColumnMapper : ColumnMapper<DatasetConfig> {
+        override fun map(r: ResultSet, columnNumber: Int, ctx: StatementContext) =
+            OBJECT_MAPPER.readValue<DatasetConfig>(r.getString(columnNumber))
     }
 
     @SqlUpdate("""
-        INSERT INTO dataset (namespace, id, config, time)
-            VALUES (:namespace, :id, :config, :time)
+        INSERT INTO dataset (namespace, id, config)
+            VALUES (:namespace, :id, :config)
+            ON CONFLICT (namespace, id)
+            DO UPDATE SET config = :config
     """)
     fun insertDataset(
         @Bind("namespace") namespace: String,
         @Bind("id") id: String,
-        @BindJson("config") config: DatasetConfig?, // Note that null indicates a tombstone
-        @Bind("time") time: Instant
+        @BindJson("config") config: DatasetConfig
+    )
+
+    @SqlUpdate("""
+        DELETE FROM dataset
+            WHERE
+                namespace = :namespace AND
+                id = :id
+    """)
+    fun deleteDataset(
+        @Bind("namespace") namespace: String,
+        @Bind("id") id: String
     )
 
     @SqlQuery("""
@@ -51,8 +64,6 @@ interface Database {
             WHERE
                 namespace = :namespace AND
                 id = :id
-            ORDER BY time DESC
-            LIMIT 1
     """)
     fun getDataset(
         @Bind("namespace") namespace: String,
@@ -60,15 +71,7 @@ interface Database {
     ): DatasetConfig?
 
     @SqlQuery("""
-        SELECT namespace, id, config FROM dataset
+        SELECT * FROM dataset
     """)
     fun getDatasets(): List<CoordinatesAndConfig>
-
-    companion object {
-        private fun parseConfigOrNull(raw: String?) = if (raw == null) {
-            null
-        } else {
-            OBJECT_MAPPER.readValue<DatasetConfig>(raw)
-        }
-    }
 }
--- catalogue/src/main/kotlin/io/quartic/catalogue/postgres/PostgresBackend.kt
+++ catalogue/src/main/kotlin/io/quartic/catalogue/postgres/PostgresBackend.kt
@@ -4,16 +4,22 @@ import com.codahale.metrics.health.HealthCheck.Result
 import io.quartic.catalogue.StorageBackend
 import io.quartic.catalogue.api.model.DatasetConfig
 import io.quartic.catalogue.api.model.DatasetCoordinates
-import java.time.Instant
 
 class PostgresBackend(private val database: Database) : StorageBackend {
     override fun get(coords: DatasetCoordinates) = database.getDataset(coords.namespace.namespace, coords.id.uid)
 
-    override fun set(coords: DatasetCoordinates, config: DatasetConfig) = insert(coords, config)
+    override fun set(coords: DatasetCoordinates, config: DatasetConfig) = database.insertDataset(
+        namespace = coords.namespace.namespace,
+        id = coords.id.uid,
+        config = config
+    )
 
     override fun contains(coords: DatasetCoordinates) = (get(coords) != null)
 
-    override fun remove(coords: DatasetCoordinates) = insert(coords, null)     // Tombstone
+    override fun remove(coords: DatasetCoordinates) = database.deleteDataset(
+        namespace = coords.namespace.namespace,
+        id = coords.id.uid
+    )
 
     override fun getAll() = database.getDatasets()
         .filter { it.config != null }
@@ -23,13 +29,4 @@ class PostgresBackend(private val database: Database) : StorageBackend {
         )
 
     override fun healthCheck(): Result = Result.healthy() // TODO
-
-    private fun insert(coords: DatasetCoordinates, config: DatasetConfig?) {
-        database.insertDataset(
-            namespace = coords.namespace.namespace,
-            id = coords.id.uid,
-            config = config,
-            time = Instant.now()
-        )
-    }
 }
