39ba070f85e165bf53131039eac03f48c406c255
Oliver Charlesworth <oliver@quartic.io>
2017-09-21 12:02:53 +0100

    Nuke Datastore (#436)
    * WIP
    
    * Get backend working
    
    * Get everything working
    
    * Write migration
    
    * Fix loose ends
    
    * Fix application test
    
    * Nuke all Datastore stuff

--- catalogue/src/main/kotlin/io/quartic/catalogue/CatalogueConfiguration.kt
+++ catalogue/src/main/kotlin/io/quartic/catalogue/CatalogueConfiguration.kt
@@ -3,7 +3,4 @@ package io.quartic.catalogue
 import io.quartic.common.application.ConfigurationBase
 import io.quartic.common.db.DatabaseConfiguration
 
-data class CatalogueConfiguration(
-    val database: DatabaseConfiguration,
-    val backend: StorageBackendConfig
-) : ConfigurationBase()
+data class CatalogueConfiguration(val database: DatabaseConfiguration) : ConfigurationBase()
