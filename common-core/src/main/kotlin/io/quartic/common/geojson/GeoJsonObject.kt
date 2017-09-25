5494fafec3e266b977f051987979e6032ee59741
Oliver Charlesworth <oliver@quartic.io>
2017-09-25 20:29:49 +0100

    Get rid of GeoJSON stuff

--- common-core/src/main/kotlin/io/quartic/common/geojson/GeoJsonObject.kt
+++
@@ -1,9 +0,0 @@
-package io.quartic.common.geojson
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-interface GeoJsonObject {
-    val crs: Map<String, Any>?
-    val bbox: List<Double>?
-}
--- common-core/src/main/kotlin/io/quartic/common/geojson/MultiLineString.kt
+++
@@ -1,7 +0,0 @@
-package io.quartic.common.geojson
-
-data class MultiLineString @JvmOverloads constructor(
-        val coordinates: List<List<List<Double>>>,
-        override val crs: Map<String, Any>? = null,
-        override val bbox: List<Double>? = null
-) : Geometry
