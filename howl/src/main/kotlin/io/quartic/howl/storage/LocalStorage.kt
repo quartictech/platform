81f8b26cbf019bd9f37ec06bdfcfe7181ffffee6
Oliver Charlesworth <oliver@quartic.io>
2017-09-25 16:19:04 +0100

    Fix tests for LocalStorage, at the expense of making it non-persistent

--- howl/src/main/kotlin/io/quartic/howl/storage/LocalStorage.kt
+++ howl/src/main/kotlin/io/quartic/howl/storage/LocalStorage.kt
@@ -1,6 +1,7 @@
 package io.quartic.howl.storage
 
-import io.quartic.howl.storage.Storage.*
+import io.quartic.howl.storage.Storage.StorageMetadata
+import io.quartic.howl.storage.Storage.StorageResult
 import io.quartic.howl.storage.StorageCoords.Managed
 import io.quartic.howl.storage.StorageCoords.Unmanaged
 import org.apache.commons.io.IOUtils
@@ -13,20 +14,27 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
+/**
+ * Ephemeral local storage.
+ */
 class LocalStorage(private val config: Config) : Storage {
     data class Config(val dataDir: String = "./data") : StorageConfig
 
+    private val contentTypes = mutableMapOf<StorageCoords, String>()
     private val lock = ReentrantReadWriteLock()
 
     override fun getData(coords: StorageCoords): StorageResult? {
         try {
             lock.readLock().lock()
+
+            val contentType = contentTypes[coords]
             val file = coords.path.toFile()
-            if (file.exists()) {
+            // Testing both is overkill, but whatever
+            if ((contentType != null) && file.exists()) {
                 return StorageResult(
                     StorageMetadata(
                         Files.getLastModifiedTime(coords.path).toInstant(),
-                        Files.probeContentType(coords.path) ?: DEFAULT_CONTENT_TYPE,
+                        contentType,
                         Files.size(coords.path)
                     ),
                     FileInputStream(file)
@@ -48,7 +56,7 @@ class LocalStorage(private val config: Config) : Storage {
             tempFile = File.createTempFile("howl", "partial")
             FileOutputStream(tempFile!!).use { fileOutputStream -> IOUtils.copy(inputStream, fileOutputStream) }
 
-            renameFile(tempFile.toPath(), coords.path)
+            commitFile(tempFile.toPath(), coords, contentType)
         } finally {
             if (tempFile != null) {
                 tempFile.delete()
@@ -64,11 +72,12 @@ class LocalStorage(private val config: Config) : Storage {
         }
     )
 
-    private fun renameFile(from: Path, to: Path) {
+    private fun commitFile(from: Path, coords: StorageCoords, contentType: String?) {
         try {
             lock.writeLock().lock()
-            Files.deleteIfExists(to)
-            Files.move(from, to)
+            Files.deleteIfExists(coords.path)
+            Files.move(from, coords.path)
+            contentTypes[coords] = contentType ?: DEFAULT_CONTENT_TYPE
         } finally {
             lock.writeLock().unlock()
         }
