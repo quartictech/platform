2480b28632f8beca0b84bfc43e8edce06c5781a5
Oliver Charlesworth <oliver@quartic.io>
2017-09-26 10:40:19 +0100

    Make install.sh modify requirements.txt in-place

--- quarty/install.sh
+++ quarty/install.sh
@@ -10,15 +10,19 @@ set -eu
 
 pip install pip-tools
 
+export CUSTOM_COMPILE_COMMAND="./gradlew createVirtualenvAndInstallDeps"
 pip-compile
 
 # Workaround for https://github.com/jazzband/pip-tools/issues/204
 # (adapted from https://github.com/jazzband/pip-tools/issues/331#issuecomment-285825098)
-sed "s|-e file://$(pwd)|-e .|" < requirements.txt > requirements.workaround.txt
+temp_file=$(mktemp)
+sed "s|-e file://$(pwd)|-e .|" < requirements.txt > ${temp_file}
+mv ${temp_file} requirements.txt
 
 # Workaround for https://github.com/jazzband/pip-tools/pull/555 (due in the next pip-tools release)
 if [ -e /etc/debian_version ]; then
     echo "pkg-resources==0.0.0" >> requirements.workaround.txt
+    pip-sync requirements.workaround.txt
+else
+    pip-sync requirements.txt
 fi
-
-pip-sync requirements.workaround.txt
