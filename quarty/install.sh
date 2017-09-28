668ed96039b2d795b89ea1d81cfdfd70290089df
Oliver Charlesworth <oliver@quartic.io>
2017-09-28 13:14:16 +0100

    Upgrade to pip-tools 1.10.1 and remove one of the hacks

--- quarty/install.sh
+++ quarty/install.sh
@@ -8,8 +8,7 @@ source ${VIRTUALENV_DIR}/bin/activate
 
 set -eu
 
-# 1.10.0 currently seems to break with "TypeError: unorderable types: InstallRequirement() < InstallRequirement()"
-pip install pip-tools==1.9.0
+pip install pip-tools==1.10.1
 
 export CUSTOM_COMPILE_COMMAND="./gradlew createVirtualenvAndInstallDeps"
 pip-compile
@@ -19,12 +18,3 @@ pip-compile
 temp_file=$(mktemp)
 sed "s|-e file://$(pwd)|-e .|" < requirements.txt > ${temp_file}
 mv ${temp_file} requirements.txt
-
-# Workaround for https://github.com/jazzband/pip-tools/pull/555 (due in the next pip-tools release)
-if [ -e /etc/debian_version ]; then
-    cp requirements.txt requirements.workaround.txt
-    echo "pkg-resources==0.0.0" >> requirements.workaround.txt
-    pip-sync requirements.workaround.txt
-else
-    pip-sync requirements.txt
-fi
