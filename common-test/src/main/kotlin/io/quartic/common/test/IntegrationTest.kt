fbb638e59c514313e14252ccd4b64ff21ac77187
Oliver Charlesworth <oliver@quartic.io>
2017-10-03 22:33:16 +0100

    GitHub integration tests (#486)
    * Get some basic GitHub API tests working
    
    * Add another test case
    
    * Add constraint on Accept header
    
    * Wire a bunch of extra tests and stuff up.
    
    * Fix HomeApplicationShould
    
    * Update README

---
+++ common-test/src/main/kotlin/io/quartic/common/test/IntegrationTest.kt
@@ -0,0 +1,7 @@
+package io.quartic.common.test
+
+/**
+ * Used to denote a test that we might one day split out into a separate "integration tests" Gradle task.
+ */
+@Target(AnnotationTarget.CLASS)
+annotation class IntegrationTest
--- github/src/main/kotlin/io/quartic/github/GitHubInstallationClient.kt
+++ github/src/main/kotlin/io/quartic/github/GitHubInstallationClient.kt
@@ -9,11 +9,12 @@ import io.quartic.common.secrets.UnsafeSecret
 import org.apache.commons.codec.binary.Base64
 import retrofit2.http.*
 import java.net.URI
-import java.security.Key
 import java.security.KeyFactory
 import java.security.spec.PKCS8EncodedKeySpec
 import java.time.Instant
 import java.util.concurrent.CompletableFuture
+import javax.ws.rs.core.HttpHeaders.ACCEPT
+import javax.ws.rs.core.HttpHeaders.AUTHORIZATION
 
 class GitHubInstallationClient(
     private val appId: String,
@@ -22,57 +23,48 @@ class GitHubInstallationClient(
     clientBuilder: ClientBuilder
 ) {
     @JsonIgnoreProperties(ignoreUnknown = true)
-    data class GitHubInstallationAccessToken(
-        val token: UnsafeSecret
-    ) {
+    data class GitHubInstallationAccessToken(val token: UnsafeSecret) {
         fun authorizationCredentials() = "token ${token.veryUnsafe}"
         fun urlCredentials() = "x-access-token:${token.veryUnsafe}"
     }
 
     @Retrofittable
     interface GitHubInstallationRetrofit {
-        @POST("/installations/{installationId}/access_tokens")
-        @retrofit2.http.Headers("Accept: application/vnd.github.machine-man-preview+json")
+        @POST("installations/{installationId}/access_tokens")
+        @retrofit2.http.Headers("${ACCEPT}: ${MEDIA_TYPE}")
         fun installationAccessTokenAsync(
             @Path("installationId") installationId: Long,
-            @Header("Authorization") auth: String
+            @Header(AUTHORIZATION) auth: String
         ): CompletableFuture<GitHubInstallationAccessToken>
 
-        @POST("/repos/{owner}/{repo}/statuses/{sha}")
-        @retrofit2.http.Headers("Accept: application/vnd.github.machine-man-preview+json")
+        @POST("repos/{owner}/{repo}/statuses/{sha}")
+        @retrofit2.http.Headers("${ACCEPT}: ${MEDIA_TYPE}")
         fun sendStatus(
             @Path("owner") owner: String,
             @Path("repo") repo: String,
             @Path("sha") sha: String,
-            @Header("Authorization") auth: String,
+            @Header(AUTHORIZATION) auth: String,
             @Body status: StatusCreate
         ): CompletableFuture<Void>
 
-        @GET("/repositories/{repoId}")
-        @retrofit2.http.Headers("Accept: application/vnd.github.machine-man-preview+json")
+        @GET("repositories/{repoId}")
+        @retrofit2.http.Headers("${ACCEPT}: ${MEDIA_TYPE}")
         fun getRepository(
             @Path("repoId") repoId: Long,
-            @Header("Authorization") auth: String
+            @Header(AUTHORIZATION) auth: String
         ): CompletableFuture<Repository>
     }
 
     private val githubRetrofit = clientBuilder.retrofit<GitHubInstallationRetrofit>(githubApiRoot)
-    private val privateKey: Key
-
-    init {
-        val encoded = Base64.decodeBase64(key.veryUnsafe)
-        val spec = PKCS8EncodedKeySpec(encoded)
-        val keyFactory = KeyFactory.getInstance("RSA")
-        privateKey = keyFactory.generatePrivate(spec)
-    }
+    private val privateKey = KeyFactory.getInstance("RSA")
+        .generatePrivate(PKCS8EncodedKeySpec(Base64.decodeBase64(key.veryUnsafe)))
 
     // See https://developer.github.com/apps/building-integrations/setting-up-and-registering-github-apps/about-authentication-options-for-github-apps/
     private fun generateJwt(): String {
         val now = Instant.now().epochSecond
         return Jwts.builder()
             .claim("iat", now)
-            // 10 minutes
-            .claim("exp", now + 10 * 60)
+            .claim("exp", now + 5 * 60) // 10-minutes is the max, but set lower to avoid rejection due to clock skew issues
             .claim("iss", appId)
             .signWith(SignatureAlgorithm.RS256, privateKey)
             .compact()
@@ -93,6 +85,10 @@ class GitHubInstallationClient(
 
     fun getRepositoryAsync(repoId: Long, accessToken: GitHubInstallationAccessToken) =
         githubRetrofit.getRepository(repoId, accessToken.authorizationCredentials())
+
+    companion object {
+        private const val MEDIA_TYPE = "application/vnd.github.machine-man-preview+json"
+    }
 }
 
 
