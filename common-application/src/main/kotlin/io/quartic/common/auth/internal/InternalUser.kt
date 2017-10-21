f389a6de455b121c34a61fb916524f9e1622b9f8
Oliver Charlesworth <oliver@quartic.io>
2017-10-21 13:29:09 +0100

    WIP

--- common-application/src/main/kotlin/io/quartic/common/auth/internal/InternalUser.kt
+++ common-application/src/main/kotlin/io/quartic/common/auth/internal/InternalUser.kt
@@ -2,7 +2,7 @@ package io.quartic.common.auth.internal
 
 import java.security.Principal
 
-data class InternalUser constructor(
+data class InternalUser(
     val id: String,                 // TODO - what does this ID actually represent?
     val namespaces: List<String>
 ) : Principal {
